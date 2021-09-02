b7dcd33e07e3cde3edabb2daac8af2650ca726f9
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-03 00:02:54 +0300

    update C# LangVersion: 10

--- Program.cs
+++ Program.cs
@@ -2,23 +2,21 @@ using Gtk;
 using ScreenFire.GUI;
 using System;
 
-namespace ScreenFire
-{
-    class Program
-    {
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            Application.Init();
+namespace ScreenFire;
 
-            Application app = new("com.nhk.ScreenFire", GLib.ApplicationFlags.None);
-            app.Register(GLib.Cancellable.Current);
+class Program {
 
-            Config config = new();
-            app.AddWindow(config);
+    [STAThread]
+    public static void Main(string[] args) {
+        Application.Init();
 
-            config.Show();
-            Application.Run();
-        }
+        Application app = new("com.nhk.ScreenFire", GLib.ApplicationFlags.None);
+        app.Register(GLib.Cancellable.Current);
+
+        Config config = new();
+        app.AddWindow(config);
+
+        config.Show();
+        Application.Run();
     }
 }
