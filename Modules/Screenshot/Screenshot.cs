b420c99c62061754621543a97e720e5d7071ad57
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-04 22:39:41 +0300

    fix class name

--- Modules/Screenshot/Screenshot.cs
+++ Modules/Screenshot/Screenshot.cs
@@ -4,14 +4,14 @@ using System;
 
 namespace ScreenFire.Modules.Screenshot;
 
-class Screens {
-    //? Example: 0f8fad5b-d9cb-469f-a165-70867728950e 
-    public Guid UniqueID { get; private set; }
+class Screenshot {
+
+    public Guid UniqueID { get; private set; } // Example: 0f8fad5b-d9cb-469f-a165-70867728950e 
 
     public ScreenMetrics ScreenMetrics { get; private set; }
     public ImageMetrics ImageMetrics { get; private set; }
 
-    public static Screens Instance(ScreenMetrics screenMetrics, ImageMetrics imageMetrics)
+    public static Screenshot Instance(ScreenMetrics screenMetrics, ImageMetrics imageMetrics)
         => new() {
             UniqueID = Guid.NewGuid(),
             ScreenMetrics = screenMetrics,
