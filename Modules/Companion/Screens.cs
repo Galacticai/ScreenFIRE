a341e2c17b0bc270d32ad0d2455bda00272786c7
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-04 19:21:04 +0300

    update the structure for upcoming code

--- Modules/Companion/Screens.cs
+++ Modules/Companion/Screens.cs
@@ -1,26 +1,49 @@
 ﻿using Gdk;
+using System.Linq;
 
 
 namespace ScreenFire.Modules.Companion;
+
 class Screens {
 
-    public int Count { get; private set; }
+    //!  PLACEHOLDER
+    private static Rectangle BoundingRectangle(Rectangle[] rectangles) {
+        int xMin = rectangles.Min(s => s.X),
+            yMin = rectangles.Min(s => s.Y),
+            xMax = rectangles.Max(s => s.X + s.Width),
+            yMax = rectangles.Max(s => s.Y + s.Height);
+        return new Rectangle(xMin, yMin, xMax - xMin, yMax - yMin);
+    }
+    //! /PLACEHOLDER
 
-    /// <summary><see cref="Rectangle"/> spanning over all screens</summary>
-    public Rectangle AllRectangle { get; private set; }
+    /// <summary> Number of displays </summary>
+    public int Count => Displays.Length;
+
+    /// <summary> <see cref="Display"/>[] of all displays</summary>
+    Display[] Displays = new DisplayManager(new System.IntPtr()).ListDisplays();
+
 
     /// <summary><see cref="Rectangle"/> array of each screen</summary> 
     public Rectangle[] Rectangles { get; private set; }
 
+    /// <summary><see cref="Rectangle"/> spanning over all screens</summary>
+    public Rectangle AllRectangle { get; private set; }
 
-    /// <returns></returns>
+
+    /// <returns> <see cref="Rectangle"/> array of each screen </returns>
     private Rectangle[] find_Rectangles() {
 
-        return Rectangles = new Rectangle[] { new(0, 0, 0, 0) }; //# PLACEHOLDER
+        return Rectangles = new Rectangle[] { new(0, 0, 0, 0) }; //! PLACEHOLDER
+    }
+
+    /// <returns> <see cref="Rectangle"/> spanning over all screens </returns>
+    private Rectangle[] find_AllRectangle() {
+
+        return Rectangles = new Rectangle[] { new(0, 0, 0, 0) }; //! PLACEHOLDER
     }
 
 
-    public static Screens instance()
+    public static Screens Instance()
         => new() {
 
         };
