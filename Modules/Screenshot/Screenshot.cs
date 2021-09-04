e1c3715b5c8abea0db30fcae8d83f08a65a9e4e3
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-04 19:22:43 +0300

    fix name rule violation

--- Modules/Screenshot/Screenshot.cs
+++ Modules/Screenshot/Screenshot.cs
@@ -1,17 +1,19 @@
-﻿using ScreenFire.Modules.Screenshot.Companion;
-
+﻿
+using ScreenFire.Modules.Screenshot.Companion;
+using System;
 
 namespace ScreenFire.Modules.Screenshot;
-class Screens {
 
-    public System.Guid UniqueID { get; private set; }
+class Screens {
+    //? Example: 0f8fad5b-d9cb-469f-a165-70867728950e 
+    public Guid UniqueID { get; private set; }
 
     public ScreenMetrics ScreenMetrics { get; private set; }
     public ImageMetrics ImageMetrics { get; private set; }
 
-    public static Screens instance(ScreenMetrics screenMetrics, ImageMetrics imageMetrics)
+    public static Screens Instance(ScreenMetrics screenMetrics, ImageMetrics imageMetrics)
         => new() {
-            UniqueID = System.Guid.NewGuid(),
+            UniqueID = Guid.NewGuid(),
             ScreenMetrics = screenMetrics,
             ImageMetrics = imageMetrics
         };
