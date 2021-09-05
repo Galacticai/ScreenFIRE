927176a6521502206c17f5b7a77f74e477eb0002
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-05 22:37:06 +0300

    add descriptions for the enum options (why tho)

--- Modules/Screenshot/Companion/IScreenshotType.cs
+++ Modules/Screenshot/Companion/IScreenshotType.cs
@@ -1,9 +1,25 @@
-﻿namespace ScreenFire.Modules.Screenshot.Companion;
+﻿using System.ComponentModel;
 
-/// <summary> Types of screenshots </summary>
+namespace ScreenFire.Modules.Screenshot.Companion;
+
+/// <summary> Types of screenshots 
+/// <list type="bullet">
+/// <item>All screens</item>
+/// <item>Screen under the mouse pointer</item>
+/// <item>Window under the mouse pointer</item>
+/// <item>Active window</item> 
+/// <item>Custom rectangle defined by the metrics</item> 
+/// </list>
+/// </summary>
 enum IScreenshotType {
+    [Description("All screens")]
     All,
+    [Description("Screen under the mouse pointer")]
     ScreenUnderMouse,
+    [Description("Window under the mouse pointer")]
     WindowUnderMouse,
+    [Description("Active window")]
+    ActiveWindow,
+    [Description("Custom rectangle defined by the metrics")]
     Custom
 }
