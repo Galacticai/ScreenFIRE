9eb43a59ac884f85096df3f01df5a2702c42a9a4
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-07 21:05:16 +0300

    init (Save locally)

---
+++ Modules/Screenshot/Save_Local.cs
@@ -0,0 +1,28 @@
+﻿using ScreenFire.Modules.Companion;
+using ScreenFire.Modules.Screenshot.Companion;
+using System;
+
+namespace ScreenFire.Modules.Screenshot;
+
+class Save_Local {
+
+    public Guid UID { get; private set; } // Example: 0f8fad5b-d9cb-469f-a165-70867728950e 
+
+    public IScreenshotType ScreenshotType { get; private set; }
+    public ISaveFormat SaveFormat { get; private set; }
+    public Screens Screens { get; private set; }
+    public ImageMetrics ImageMetrics { get; private set; }
+
+    //! Save location
+    //! -- File Name
+    //! -- Folder
+    //! 
+    //! ?? etc
+
+    public static Save_Local Instance(ScreenshotInfo screenshot,
+                                      ISaveFormat saveFormat = ISaveFormat.PNG)
+        => new() {
+            UID = screenshot.UID,
+            SaveFormat = saveFormat
+        };
+}
