148df39f74c9a36e5bddcc52107fc65ddeed84be
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-07 21:02:11 +0300

    init (Saving format of the `Screenshot`)

---
+++ Modules/Screenshot/Companion/ISaveFormat.cs
@@ -0,0 +1,6 @@
+﻿namespace ScreenFire.Modules.Screenshot.Companion;
+
+/// <summary> Saving format of the <see cref="Screenshot"/> </summary>
+enum ISaveFormat {
+    BMP, PNG, JPG //? Maybe later => GIF, MP4, ... MP3 ??
+}
