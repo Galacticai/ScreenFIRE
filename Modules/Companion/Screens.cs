716120f40f444bcc77b602d37e4e8b6dc2ed537e
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-07 12:29:25 +0300

    update: real code

--- Modules/Companion/Screens.cs
+++ Modules/Companion/Screens.cs
@@ -1,41 +1,56 @@
 ﻿using Gdk;
+using ScreenFire.Modules.Companion.math;
+using System.Collections.Generic;
 
 namespace ScreenFire.Modules.Companion;
 
-
+/// <summary> Screens rectangles </summary>
 class Screens {
 
-    //!  PLACEHOLDER
-    //! /PLACEHOLDER
-
     /// <summary> Number of displays </summary>
-    public int Count => Displays.Length;
+    public int Count => Rectangles == null ? Rectangles.Count : throw new System.NullReferenceException();
 
-    /// <summary> <see cref="Display"/>[] of all displays</summary>
-    Display[] Displays = new DisplayManager(new System.IntPtr()).ListDisplays();
-
-
+    private bool _RectanglesAssigned = false;
     /// <summary><see cref="Rectangle"/> array of each screen</summary> 
-    public Rectangle[] Rectangles { get; private set; }
+    public List<Rectangle> Rectangles { get; private set; }
 
     /// <summary><see cref="Rectangle"/> spanning over all screens</summary>
     public Rectangle AllRectangle { get; private set; }
 
 
-    /// <returns> <see cref="Rectangle"/> array of each screen </returns>
-    private Rectangle[] find_Rectangles() {
 
-        return Rectangles = new Rectangle[] { new(0, 0, 0, 0) }; //! PLACEHOLDER
+    /// <returns> <see cref="Rectangle"/> array of each screen </returns>
+    private List<Rectangle> find_Rectangles() {
+        Rectangles.Clear(); //? Clear before adding
+        for (int i = 0; i < Count; i++)
+            Rectangles.Add(new Display(new()).GetMonitor(i).Geometry);
+        return Rectangles;
     }
 
+
+    /// <summary> AUTO </summary>
     /// <returns> <see cref="Rectangle"/> spanning over all screens </returns>
     private Rectangle find_AllRectangle()
-            => AllRectangle = math.Vision.Geometry.BoundingRectangle(Rectangles); //! PLACEHOLDER
+            => AllRectangle = Vision.Geometry.BoundingRectangle(find_Rectangles().ToArray());
+
+    /// <summary> MANUAL </summary>
+    /// <returns> <see cref="Rectangle"/> spanning over all screens </returns>
+    private Rectangle find_AllRectangle(Rectangle[] rectangles)
+            => AllRectangle = Vision.Geometry.BoundingRectangle(rectangles);
 
 
 
-    public static Screens New()
-        => new() {
+    /// <summary> AUTO </summary>
+    public Screens Instance()
+            => new() {
+                AllRectangle = find_AllRectangle(), //! It updaes both props
+                Rectangles = Rectangles //? No need to find, already done above
+            };
 
-        };
+    /// <summary> MANUAL </summary>
+    public Screens Instance(List<Rectangle> rectangles)
+            => new() {
+                AllRectangle = find_AllRectangle(rectangles),
+                Rectangles = rectangles
+            };
 }
