e8216f46c974a9142af7e84201ef3477b835ff6a
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-07 21:08:15 +0300

    rename to ScreenshotInfo & use `ISaveFormat` (while saving) instead of `ImageFormat` (while firing)

--- Modules/Screenshot/ScreenshotInfo.cs
+++ Modules/Screenshot/ScreenshotInfo.cs
@@ -1,32 +1,24 @@
 ﻿using ScreenFire.Modules.Companion;
 using ScreenFire.Modules.Screenshot.Companion;
 using System;
-using System.Drawing.Imaging;
 
 namespace ScreenFire.Modules.Screenshot;
 
-class Screenshot {
+class ScreenshotInfo {
 
     public Guid UID { get; private set; } // Example: 0f8fad5b-d9cb-469f-a165-70867728950e 
 
     public IScreenshotType ScreenshotType { get; private set; }
-    public ImageFormat ImageFormat { get; private set; }
     public Screens Screens { get; private set; }
     public ImageMetrics ImageMetrics { get; private set; }
 
-    public static Screenshot Instance(IScreenshotType screenshotType,
-                                 ImageMetrics imageMetrics,
-                                 ImageFormat imageFormat = null) {
-
-        if (imageFormat == null) imageFormat = ImageFormat.Png;
-
-
+    public static ScreenshotInfo Instance(IScreenshotType screenshotType,
+                                 ImageMetrics imageMetrics) {
 
         return new() {
             UID = Guid.NewGuid(),
             ScreenshotType = screenshotType,
-            ImageMetrics = imageMetrics,
-            ImageFormat = imageFormat
+            ImageMetrics = imageMetrics
         };
     }
 }
