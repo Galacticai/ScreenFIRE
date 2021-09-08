0ce85028472dfdb8a468c4f3bcad5f38fcf60c8a
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-08 23:41:50 +0300

    use block-scoped namespaces & update capitalize `FIRE` in `ScreenFIRE`

--- GUI/Config.cs
+++ GUI/Config.cs
@@ -1,4 +1,3 @@
-
 using Gdk;
 using Gtk;
 using System;
@@ -6,52 +5,53 @@ using System.Drawing.Imaging;
 using sysD = System.Drawing;
 using UI = Gtk.Builder.ObjectAttribute;
 
-namespace ScreenFire.GUI;
+namespace ScreenFIRE.GUI {
 
-class Config : Gtk.Window {
-    [UI] private Label _label1 = null;
-    [UI] private Button ss_Button = null;
+    class Config : Gtk.Window {
+        [UI] private Label _label1 = null;
+        [UI] private Button ss_Button = null;
 
-    private void AssignEvents() {
-        DeleteEvent += Window_DeleteEvent;
-        ss_Button.Clicked += ss_Button_Clicked;
-    }
+        private void AssignEvents() {
+            DeleteEvent += Window_DeleteEvent;
+            ss_Button.Clicked += ss_Button_Clicked;
+        }
 
-    public Config() : this(new Builder("Config.glade")) { }
+        public Config() : this(new Builder("Config.glade")) { }
 
-    private Config(Builder builder) : base(builder.GetRawOwnedObject("Config")) {
-        builder.Autoconnect(this);
+        private Config(Builder builder) : base(builder.GetRawOwnedObject("Config")) {
+            builder.Autoconnect(this);
 
-        AssignEvents();
-    }
+            AssignEvents();
+        }
 
-    private void Window_DeleteEvent(object sender, DeleteEventArgs ev)
-            => Application.Quit();
+        private void Window_DeleteEvent(object sender, DeleteEventArgs ev)
+                => Application.Quit();
 
-    private int _counter;
-    private void ss_Button_Clicked(object sender, EventArgs ev) {
-        sysD.Image ss;
-        Rectangle monitorGeo = Display.GetMonitor(0).Geometry;
-        if (monitorGeo.Width > 0 & monitorGeo.Height > 0)
-            ss = ScreenFire.Modules.Companion.math.Vision.Screenshot(new(0, 0, monitorGeo.Width, monitorGeo.Height));
-        else throw new ArgumentOutOfRangeException("Fatal Error: Monitor geometry is out of range.");
+        private int _counter;
+        private void ss_Button_Clicked(object sender, EventArgs ev) {
+            sysD.Image ss;
+            Rectangle monitorGeo = Display.GetMonitor(0).Geometry;
+            if (monitorGeo.Width > 0 & monitorGeo.Height > 0)
+                ss = ScreenFIRE.Modules.Companion.math.Vision.Screenshot(new(0, 0, monitorGeo.Width, monitorGeo.Height));
+            else throw new ArgumentOutOfRangeException("Fatal Error: Monitor geometry is out of range.");
 
-        FileChooserDialog fcd = new("Save the fire", null, FileChooserAction.Save);
-        fcd.AddButton(Stock.Cancel, ResponseType.Cancel);
-        fcd.AddButton(Stock.Save, ResponseType.Ok);
-        fcd.DefaultResponse = ResponseType.Ok;
-        fcd.SelectMultiple = false;
+            FileChooserDialog fcd = new("Save the fire", null, FileChooserAction.Save);
+            fcd.AddButton(Stock.Cancel, ResponseType.Cancel);
+            fcd.AddButton(Stock.Save, ResponseType.Ok);
+            fcd.DefaultResponse = ResponseType.Ok;
+            fcd.SelectMultiple = false;
 
-        ResponseType response = (ResponseType)fcd.Run();
+            ResponseType response = (ResponseType)fcd.Run();
 
-        if (response == ResponseType.Ok
-           & System.IO.Directory.Exists(fcd.CurrentFolder)
-           & ss != null)
-            ss.Save((fcd.Filename.ToLower().Contains(".png") ? fcd.Filename : $"{fcd.Filename}.png"),
-                    ImageFormat.Png);
+            if (response == ResponseType.Ok
+               & System.IO.Directory.Exists(fcd.CurrentFolder)
+               & ss != null)
+                ss.Save((fcd.Filename.ToLower().Contains(".png") ? fcd.Filename : $"{fcd.Filename}.png"),
+                        ImageFormat.Png);
 
-        fcd.Destroy();
+            fcd.Destroy();
 
-        _label1.Text = $"Fired a Screenshot!\n\nThis button has been clicked {(1 + (_counter++))} time{(_counter > 1 ? "s" : "")}.";
+            _label1.Text = $"Fired a Screenshot!\n\nThis button has been clicked {(1 + (_counter++))} time{(_counter > 1 ? "s" : "")}.";
+        }
     }
 }
