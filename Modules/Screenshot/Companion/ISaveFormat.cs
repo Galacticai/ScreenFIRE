92170b362041d585901edc41927b4218457e6373
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-08 23:47:41 +0300

    use block-scoped namespaces

--- Modules/Screenshot/Companion/ISaveFormat.cs
+++ Modules/Screenshot/Companion/ISaveFormat.cs
@@ -1,17 +1,17 @@
-﻿
-namespace ScreenFire.Modules.Screenshot.Companion;
+﻿namespace ScreenFIRE.Modules.Screenshot.Companion {
 
-/// <summary> Saving format of the <see cref="ScreenshotInfo"/> 
-/// <list type="bullet">
-/// <item> 0 • bmp </item>
-/// <item> 1 • png </item>
-/// <item> 2 • jpg </item>
-/// <item> ( Maybe later => gif, mp4, ... mp3 ?? ) </item>
-/// </list>
-/// </summary>
-enum ISaveFormat {
-    bmp,
-    png,
-    jpg
-    //? Maybe later => gif, mp4, ... mp3 ??
+    /// <summary> Saving format of the <see cref="ScreenshotInfo"/> 
+    /// <list type="bullet">
+    /// <item> 0 • bmp </item>
+    /// <item> 1 • png </item>
+    /// <item> 2 • jpg </item>
+    /// <item> ( Maybe later => gif, mp4, ... mp3 ?? ) </item>
+    /// </list>
+    /// </summary>
+    enum ISaveFormat {
+        bmp,
+        png,
+        jpg
+        //? Maybe later => gif, mp4, ... mp3 ??
+    }
 }
--- Modules/Screenshot/Companion/IScreenshotType.cs
+++ Modules/Screenshot/Companion/IScreenshotType.cs
@@ -1,19 +1,19 @@
-﻿
-namespace ScreenFire.Modules.Screenshot.Companion;
+﻿namespace ScreenFIRE.Modules.Screenshot.Companion {
 
-/// <summary> Types of screenshots 
-/// <list type="bullet">
-/// <item>All screens</item>
-/// <item>Screen under the mouse pointer</item>
-/// <item>Window under the mouse pointer</item>
-/// <item>Active window</item> 
-/// <item>Custom rectangle defined by the metrics</item> 
-/// </list>
-/// </summary>
-enum IScreenshotType {
-    All,
-    ScreenUnderMouse,
-    WindowUnderMouse,
-    ActiveWindow,
-    Custom
+    /// <summary> Types of screenshots 
+    /// <list type="bullet">
+    /// <item>All screens</item>
+    /// <item>Screen under the mouse pointer</item>
+    /// <item>Window under the mouse pointer</item>
+    /// <item>Active window</item> 
+    /// <item>Custom rectangle defined by the metrics</item> 
+    /// </list>
+    /// </summary>
+    enum IScreenshotType {
+        All,
+        ScreenUnderMouse,
+        WindowUnderMouse,
+        ActiveWindow,
+        Custom
+    }
 }
