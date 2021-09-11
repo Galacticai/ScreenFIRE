17ad8a1760a6ec93fdeaaf200f855ed789da4f2c
NHKomaiha <nhkomaiha@gmail.com>
2021-09-11 16:14:26 +0300

    init (common class)

---
+++ Modules/Companion/c.cs
@@ -0,0 +1,11 @@
+﻿namespace ScreenFIRE.Modules.Companion {
+
+    /// <summary> Common class for general ScreenFIRE stuff </summary>
+    class c {
+
+        public static string SF => "";
+
+        public static string User_SF => "";
+
+    }
+}
