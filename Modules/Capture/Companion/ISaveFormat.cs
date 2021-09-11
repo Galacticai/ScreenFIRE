375b68e78b859c3be037b46529d39e18ab44da76
NHKomaiha <nhkomaiha@gmail.com>
2021-09-11 16:12:19 +0300

    add `SaveFormat.ToImageFormat(...)`

--- Modules/Capture/Companion/ISaveFormat.cs
+++ Modules/Capture/Companion/ISaveFormat.cs
@@ -1,4 +1,7 @@
-﻿namespace ScreenFIRE.Modules.Capture.Companion {
+﻿using System.Drawing.Imaging;
+
+namespace ScreenFIRE.Modules.Capture.Companion
+{
 
     /// <summary> Saving format of the <see cref="ScreenshotInfo"/> 
     /// <list type="bullet">
@@ -8,10 +11,24 @@
     /// <item> ( Maybe later => gif, mp4, ... mp3 ?? ) </item>
     /// </list>
     /// </summary>
-    enum ISaveFormat {
+    enum ISaveFormat
+    {
         bmp,
         png,
         jpg
         //? Maybe later => gif, mp4, ... mp3 ??
+    }
+
+    static class SaveFormat
+    {
+        public static ImageFormat ToImageFormat(ISaveFormat saveFormat)
+            => saveFormat switch
+            {
+                ISaveFormat.bmp => ImageFormat.Bmp,
+                ISaveFormat.png => ImageFormat.Png,
+                ISaveFormat.jpg => ImageFormat.Jpeg,
+
+                _ => ImageFormat.Png,
+            };
     }
 }
