79706756a18c78571997b17bc79d0cd45cd92adb
NHKomaiha <nhkomaiha@gmail.com>
2021-09-11 16:17:06 +0300

    fix bug: null `Gdk.Display` & update naming scheme

--- Modules/Companion/Screens.cs
+++ Modules/Companion/Screens.cs
@@ -2,13 +2,15 @@
 using System.Drawing;
 using g = Gdk;
 
-namespace ScreenFIRE.Modules.Companion {
+namespace ScreenFIRE.Modules.Companion
+{
 
     /// <summary> Screens rectangles </summary>
-    class Screens {
+    class Screens
+    {
 
         /// <summary> Number of monitors </summary>
-        public int Count => new g.Display(new()).NMonitors;
+        public static int Count => g.Display.Default.NMonitors;
 
         /// <summary> <see cref="Rectangle"/> array of each screen</summary> 
         public Rectangle[] Rectangles { get; private set; }
@@ -19,15 +21,14 @@ namespace ScreenFIRE.Modules.Companion {
 
 
         /// <returns> <see cref=" Rectangle"/> array of each screen </returns>
-        private Rectangle[] find_Rectangles() {
-            var display = new g.Display(new());
-
-            Rectangles = new Rectangle[Count]; //? Reset  
-
-            g.Rectangle gRect;  // outside. to prevent redeclaring every loop  
-            for (int i = 0; i < Count; i++) {
-                gRect = display.GetMonitor(i).Geometry;
-                Rectangles[i] = new(gRect.X, gRect.Y, gRect.Width, gRect.Height);// convert from Gdk to System.Drawing
+        private Rectangle[] Rectangles_Auto()
+        {
+            Rectangles = new Rectangle[Count]; //? Reset
+            g.Rectangle gRect;  // outside || Prevent redeclaring every loop  
+            for (int i = 0; i < Count; i++)
+            {
+                gRect = g.Display.Default.GetMonitor(i).Geometry;
+                Rectangles[i] = new(gRect.X, gRect.Y, gRect.Width, gRect.Height); // convert from Gdk to System.Drawing
             }
             return Rectangles;
         }
@@ -35,27 +36,29 @@ namespace ScreenFIRE.Modules.Companion {
 
         /// <summary> AUTO </summary>
         /// <returns> <see cref="Rectangle"/> spanning over all screens </returns>
-        private Rectangle find_AllRectangle()
+        private Rectangle AllRectangle_Auto()
                 => AllRectangle = Vision.Geometry.BoundingRectangle(find_Rectangles());
 
         /// <summary> MANUAL </summary>
         /// <returns> <see cref="Rectangle"/> spanning over all screens </returns>
-        private Rectangle find_AllRectangle(Rectangle[] rectangles)
+        private Rectangle AllRectangle_Auto(Rectangle[] rectangles)
                 => AllRectangle = Vision.Geometry.BoundingRectangle(rectangles);
 
 
 
         /// <summary> AUTO </summary>
-        public Screens Instance()
-                => new() {
-                    AllRectangle = find_AllRectangle(), //! It updaes both props
+        public Screens New()
+                => new()
+                {
+                    AllRectangle = AllRectangle_Auto(), //! It updaes both props
                     Rectangles = Rectangles //? No need to find, already done above
                 };
 
         /// <summary> MANUAL </summary>
-        public Screens Instance(Rectangle[] rectangles)
-                => new() {
-                    AllRectangle = find_AllRectangle(rectangles),
+        public Screens New(Rectangle[] rectangles)
+                => new()
+                {
+                    AllRectangle = AllRectangle_Auto(rectangles),
                     Rectangles = rectangles
                 };
     }
