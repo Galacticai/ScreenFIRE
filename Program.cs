0cd9190a4b19e53b86ed160469f39c32f2971c39
NHKomaiha <nhkomaiha@gmail.com>
2021-09-11 16:19:05 +0300

    use block-scope namespace

--- Program.cs
+++ Program.cs
@@ -2,23 +2,27 @@ using Gtk;
 using ScreenFIRE.GUI;
 using System;
 
-namespace ScreenFIRE;
+namespace ScreenFIRE
+{
 
-class Program {
+    class Program
+    {
 
-    public const string packageName = "com.nhk.ScreenFIRE";
+        public const string packageName = "com.nhk.ScreenFIRE";
 
-    [STAThread]
-    public static void Main(string[] args) {
-        Application.Init();
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.Init();
 
-        Application ScreenFIRE = new(packageName, GLib.ApplicationFlags.None);
-        ScreenFIRE.Register(GLib.Cancellable.Current);
+            Application ScreenFIRE = new(packageName, GLib.ApplicationFlags.None);
+            ScreenFIRE.Register(GLib.Cancellable.Current);
 
-        Config ScreenFIRE_config = new();
-        ScreenFIRE.AddWindow(ScreenFIRE_config);
+            Config ScreenFIRE_config = new();
+            ScreenFIRE.AddWindow(ScreenFIRE_config);
 
-        ScreenFIRE_config.Show();
-        Application.Run();
+            ScreenFIRE_config.Show();
+            Application.Run();
+        }
     }
 }
