774f210eb0b77e3328c9ed9373164c837beaf367
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-12 23:06:23 +0300

    init (Prepare the environment for ScreenFIRE)

---
+++ Modules/Companion/PrepareEnvironment.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace ScreenFIRE.Modules.Companion {
+
+    /// <summary> Prepare the environment for ScreenFIRE </summary>
+    internal static class PrepareEnvironment {
+        public static void Run() {
+            if (!Directory.Exists(c.SF)) Directory.CreateDirectory(c.SF);
+            if (!Directory.Exists(c.SF_Data)) Directory.CreateDirectory(c.SF_Data);
+        }
+    }
+}
