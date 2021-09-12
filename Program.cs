596d5bf3b796be164b95afdd6199b51c346e3dfe
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-12 16:28:32 +0300

    check platform ID before proceeding

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 using Gtk;
 using ScreenFIRE.GUI;
+using ScreenFIRE.Modules.Companion;
 using System;
 
-namespace ScreenFIRE
-{
+namespace ScreenFIRE {
 
-    class Program
-    {
+    class Program {
 
         public const string packageName = "com.nhk.ScreenFIRE";
 
+        private static bool PlatformIsSupported
+            => Environment.OSVersion.Platform == PlatformID.Unix
+             | Environment.OSVersion.Platform == PlatformID.Win32NT;
+
         [STAThread]
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) {
+            if (!PlatformIsSupported)
+                throw new PlatformNotSupportedException(
+                                $"Sorry ScreenFIRE does not support Platform ID \"{Environment.OSVersion.Platform}\""
+                                + $"{c.n}Please run ScreenFIRE on Windows or Linux.");
+
             Application.Init();
 
             Application ScreenFIRE = new(packageName, GLib.ApplicationFlags.None);
