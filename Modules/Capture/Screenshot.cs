08b627e60194997fd5db9846c0f7e3ec5c651403
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-14 20:38:24 +0300

    add `Time` property & use `Gdk.Rectangle` instead of `System.Drawing` & add xaml doc

--- Modules/Capture/Screenshot.cs
+++ Modules/Capture/Screenshot.cs
@@ -1,21 +1,28 @@
 ﻿using ScreenFIRE.Modules.Capture.Companion;
 using System;
-using System.Drawing;
 
 namespace ScreenFIRE.Modules.Capture {
 
     class Screenshot {
 
-        public Guid UID { get; private set; } // Example: 0f8fad5b-d9cb-469f-a165-70867728950e 
+        /// <summary> 
+        /// Unique ID specific for this screenshot <br/><br/>
+        /// >> Example: 0f8fad5b-d9cb-469f-a165-70867728950e 
+        /// </summary>
+        public Guid UID { get; private set; }
+
+        /// <summary> Date and time of screen firing </summary>
+        public DateTime Time { get; private set; }
 
         public IScreenshotType ScreenshotType { get; private set; }
-        public Rectangle ImageRectangle { get; private set; }
+        public Gdk.Rectangle ImageRectangle { get; private set; }
         //public Screens Screens { get; private set; }
 
         public static Screenshot New(IScreenshotType screenshotType,
-                                     Rectangle imageRectangle)
+                                     Gdk.Rectangle imageRectangle)
             => new() {
                 UID = Guid.NewGuid(),
+                Time = DateTime.Now,
                 ScreenshotType = screenshotType,
                 ImageRectangle = imageRectangle
             };
