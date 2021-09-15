b0fde9e111455c7b85fb845067ffe65d8a2fdf58
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-15 22:38:50 +0300

    init (Determine system language)

---
+++ Modules/Companion/Language.cs
@@ -0,0 +1,33 @@
+﻿using cult = System.Globalization.CultureInfo;
+
+namespace ScreenFIRE.Modules.Companion {
+
+    /// <summary>
+    /// <list type="bullet">
+    /// <item> 0 • Other • Any language not listed </item>
+    /// <item> 1 • English </item>
+    /// <item> 2 • Arabic </item>
+    /// <item> 3 • Chinese </item>
+    /// </list>
+    /// </summary>
+    public enum ILanguage {
+        Other,
+
+        English,
+        //! Later
+        Arabic,
+        Chinese
+    }
+
+    public class Language {
+        public static ILanguage GetSystemLanguage()
+            => cult.InstalledUICulture.TwoLetterISOLanguageName switch {
+                "en" => ILanguage.English,
+                "ar" => ILanguage.Arabic,
+                "zh" => ILanguage.Chinese,
+
+                _ => ILanguage.Other
+            };
+    }
+
+}
