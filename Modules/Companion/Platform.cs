0b78e3a7dc03c42066ac810ad31aaf6e98aea6bf
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-15 22:39:22 +0300

    move from `Common.cs` to a separate class

---
+++ Modules/Companion/Platform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ScreenFIRE.Modules.Companion {
+
+    /// <summary> Common class for general ScreenFIRE stuff </summary>
+    static class Platform {
+
+
+        private static string CurrentLang => CultureInfo.InstalledUICulture.TwoLetterISOLanguageName;
+
+
+        public static bool CurrentLangIsEnglish => CurrentLang == "en";
+        public static bool CurrentLangIsArabic => CurrentLang == "ar";
+        public static bool CurrentLangIsChinese => CurrentLang == "zh";
+
+
+        /// <summary> Indicate if the current platform is Unix based </summary>
+        public static bool RunningLinux
+            => Environment.OSVersion.Platform == PlatformID.Unix;
+
+        /// <summary> Indicate if the current platform is Win32NT based (Windows NT and above) </summary>
+        public static bool RunningWindows
+            => Environment.OSVersion.Platform == PlatformID.Win32NT;
+
+    }
+}
