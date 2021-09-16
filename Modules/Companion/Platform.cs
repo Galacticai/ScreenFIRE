051a9482b842b226d69072e6e6a6543413a0be3b
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-16 14:14:42 +0300

    move language-related stuff to `Language.cs`

--- Modules/Companion/Platform.cs
+++ Modules/Companion/Platform.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Globalization;
 
 
 namespace ScreenFIRE.Modules.Companion {
 
     /// <summary> Common class for general ScreenFIRE stuff </summary>
     static class Platform {
-
-
-        private static string CurrentLang => CultureInfo.InstalledUICulture.TwoLetterISOLanguageName;
-
-
-        public static bool CurrentLangIsEnglish => CurrentLang == "en";
-        public static bool CurrentLangIsArabic => CurrentLang == "ar";
-        public static bool CurrentLangIsChinese => CurrentLang == "zh";
-
 
         /// <summary> Indicate if the current platform is Unix based </summary>
         public static bool RunningLinux
