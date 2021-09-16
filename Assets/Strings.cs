705ee9c92c532711246c58adf3bffbaaac39c860
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-16 14:15:37 +0300

    init (Get localized strings with ease)

--- Assets/Strings.cs
+++ Assets/Strings.cs
@@ -1,7 +1,55 @@
-﻿namespace ScreenFIRE.Assets {
+﻿using ScreenFIRE.Modules.Companion;
+
+namespace ScreenFIRE.Assets {
+
+    public enum IStrings {
+
+        ScreenFIRE
+
+    }
 
     public record struct Strings {
+        /// <summary> Fetch a specific string </summary>
+        /// <param name="name"></param>
+        /// <returns>Localized string according to requested language / or system language if not specified</returns>
+        public static string Fetch(IStrings name)//, ILanguage language = ILanguage.System)
+             => Language.GetSystemLanguage() switch {
+                 ILanguage.Arabic => Ar(name),
+                 ILanguage.Chinese => Zh(name),
 
+                 //! System / English / Other
+                 _ => En(name),
+             };
+
+
+        #region Localized strings
+
+        private static string En(IStrings Name)
+          => Name switch {
+              //? name => "value"
+
+              IStrings.ScreenFIRE => "ScreenFIRE",
+
+              _ => string.Empty, //! PLACEHOLDER
+          };
+
+        private static string Ar(IStrings Name)
+          => Name switch {
+
+              IStrings.ScreenFIRE => "حريق الشاشة ScreenFIRE",
+
+              _ => string.Empty, //! PLACEHOLDER
+          };
+
+        private static string Zh(IStrings Name)
+          => Name switch {
+
+              IStrings.ScreenFIRE => "屏幕火 ScreenFIRE",
+
+              _ => string.Empty, //! PLACEHOLDER
+          };
+
+        #endregion
 
 
     }
