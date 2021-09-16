09f49be1aeaf307ed54e2d2552905399699c77ca
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-16 14:15:13 +0300

    rearrange languages & add System option

--- Modules/Companion/Language.cs
+++ Modules/Companion/Language.cs
@@ -4,24 +4,36 @@ namespace ScreenFIRE.Modules.Companion {
 
     /// <summary>
     /// <list type="bullet">
-    /// <item> 0 • Other • Any language not listed </item>
+    /// <item> 0 • System • Current system language (<see cref="English"/> if system language not supported) </item>
     /// <item> 1 • English </item>
     /// <item> 2 • Arabic </item>
     /// <item> 3 • Chinese </item>
+    /// <item> 4 • Other • Any language not listed </item>
     /// </list>
     /// </summary>
     public enum ILanguage {
-        Other,
+        System,
 
         English,
-        //! Later
         Arabic,
-        Chinese
+        Chinese,
+
+        Other,
     }
 
     public class Language {
+
+        public static string TwoLetterISOLanguageName
+            => cult.InstalledUICulture.TwoLetterISOLanguageName;
+
+        #region Shortcuts
+        public static bool CurrentLangIsEnglish => TwoLetterISOLanguageName == "en";
+        public static bool CurrentLangIsArabic => TwoLetterISOLanguageName == "ar";
+        public static bool CurrentLangIsChinese => TwoLetterISOLanguageName == "zh";
+        #endregion
+
         public static ILanguage GetSystemLanguage()
-            => cult.InstalledUICulture.TwoLetterISOLanguageName switch {
+            => TwoLetterISOLanguageName switch {
                 "en" => ILanguage.English,
                 "ar" => ILanguage.Arabic,
                 "zh" => ILanguage.Chinese,
