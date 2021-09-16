6c745d479fee65d6c021979f006f57af5ad56a48
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-16 14:30:01 +0300

    change properties `set` accessor to `init` (immutable)

--- Modules/Capture/Screenshot.cs
+++ Modules/Capture/Screenshot.cs
@@ -9,13 +9,13 @@ namespace ScreenFIRE.Modules.Capture {
         /// Unique ID specific for this screenshot <br/><br/>
         /// >> Example: 0f8fad5b-d9cb-469f-a165-70867728950e 
         /// </summary>
-        public Guid UID { get; private set; }
+        public Guid UID { get; init; }
 
         /// <summary> Date and time of screen firing </summary>
-        public DateTime Time { get; private set; }
+        public DateTime Time { get; init; }
 
-        public IScreenshotType ScreenshotType { get; private set; }
-        public Gdk.Rectangle ImageRectangle { get; private set; }
+        public IScreenshotType ScreenshotType { get; init; }
+        public Gdk.Rectangle ImageRectangle { get; init; }
         //public Screens Screens { get; private set; }
 
         public Screenshot(IScreenshotType screenshotType,
