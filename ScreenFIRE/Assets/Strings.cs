23e4695d064eea902bb7a4cb7a3a71c542a8c11a
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-18 23:15:12 +0300

    add `Fetch` overload (multiple strings support) + `FetchAndJoin` use underscore to indicate special characters & add a bunch of strings

--- ScreenFIRE/Assets/Strings.cs
+++ ScreenFIRE/Assets/Strings.cs
@@ -1,28 +1,50 @@
 ﻿using ScreenFIRE.Modules.Companion;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace ScreenFIRE.Assets {
 
     public enum IStrings {
         ScreenFIRE,
 
-        dot,
+        ChooseHowYouWouldLikeToFireYourScreen_,
+        FiredAScreenshot_,
+        ThisButtonHasBeenClicked,
+        times_1,
+        times_2,
 
-        SaveAs,
-        FileAlreadyExists,
+        AllMonitors,
+        MonitorAtPointer,
+        WindowAtPointer,
+        ActiveWindow,
+
+        SaveAs___,
+        FileAlreadyExists_,
         alreadyExists,
-        DoYouWantToReplaceTheExistingFile,
+        WouldYouLikeToReplaceTheExistingFile_,
 
     }
 
     public record Strings {
 
+        public static string FetchAndJoin(params IStrings[] Names) {
+            return string.Join(" ", Fetch(Names));
+        }
 
+        /// <summary> Fetch a set of strings </summary>
+        /// <param name="Name"> String names provided by <see cref="IStrings"/> </param>
+        /// <returns> Localized <see cref="string"/>[] array according to system language </returns>
+        public static string[] Fetch(params IStrings[] Names) {
+            List<string> result = new();
+            foreach (var name in Names)
+                result.Add(Fetch(name,
+                                  false)); //! skip translation when fetching multiple strings
+            return result.ToArray();/*await Languages.TranslateText(result, language)*/ //! translate all at once
+        }
 
         /// <summary> Fetch a specific string </summary>
         /// <param name="Name"> String name provided by <see cref="IStrings"/> </param>
-        /// <returns> Localized string according to requested language / or system language if not specified </returns>
-        public static async Task<string> Fetch(IStrings Name) {
+        /// <returns> Localized <see cref="string"/> according to system language </returns>
+        public static string Fetch(IStrings Name, bool translate = true) {
             ILanguages language = Languages.DotNetToILanguages();
             return language switch { //! System language
                 //ILanguages.English => En(Name),
@@ -30,7 +52,7 @@ namespace ScreenFIRE.Assets {
                 //ILanguages.Chinese => Zh(name),
 
                 //! English / Other
-                _ => await Languages.TranslateText(En(Name), ILanguages.Arabic)// language)
+                _ => translate ? En(Name)/*await Languages.TranslateText(En(Name), language)*/ : En(Name),
             };
         }
 
@@ -41,12 +63,21 @@ namespace ScreenFIRE.Assets {
           => Name switch {
               IStrings.ScreenFIRE => "ScreenFIRE",
 
-              IStrings.dot => ".",
+              IStrings.ChooseHowYouWouldLikeToFireYourScreen_ => "Choose how you would like to to fire your screen!",
+              IStrings.FiredAScreenshot_ => "Fired a screenshot!",
+              IStrings.ThisButtonHasBeenClicked => "This button has been clicked",
+              IStrings.times_1 => "time",
+              IStrings.times_2 => "times",
 
-              IStrings.SaveAs => "Save As",
-              IStrings.FileAlreadyExists => "File already exists",
-              IStrings.alreadyExists => "already exists.",
-              IStrings.DoYouWantToReplaceTheExistingFile => "Do you want to replace the existing file?",
+              IStrings.AllMonitors => "All monitors",
+              IStrings.MonitorAtPointer => "Monitor at pointer",
+              IStrings.WindowAtPointer => "Window at pointer",
+              IStrings.ActiveWindow => "Active window",
+
+              IStrings.SaveAs___ => "Save as...",
+              IStrings.FileAlreadyExists_ => "File already exists.",
+              IStrings.alreadyExists => "already exists",
+              IStrings.WouldYouLikeToReplaceTheExistingFile_ => "Would you like to replace the existing file?",
 
 
               //!? Last resort
@@ -57,12 +88,21 @@ namespace ScreenFIRE.Assets {
           => Name switch {
               IStrings.ScreenFIRE => "حريق الشاشة ScreenFIRE",
 
-              IStrings.dot => ".",
+              IStrings.ChooseHowYouWouldLikeToFireYourScreen_ => "اختر كيف تود الطلق على شاشتك!",
+              IStrings.FiredAScreenshot_ => "تم اطلاق صورة الشاشة!",
+              IStrings.ThisButtonHasBeenClicked => "هذا الزر قد ضغط",
+              IStrings.times_1 => "مرة",
+              IStrings.times_2 => "مرات",
 
-              IStrings.SaveAs => "حفظ باسم",
-              IStrings.FileAlreadyExists => "الملف موجود مسبقاً.",
-              IStrings.alreadyExists => "موجود مسبقاً.",
-              IStrings.DoYouWantToReplaceTheExistingFile => "هل تودّ استبدال الملف السابق؟",
+              IStrings.AllMonitors => "جميع الشاشات",
+              IStrings.MonitorAtPointer => "الشاشة عند المؤشر",
+              IStrings.WindowAtPointer => "النافذة عند المؤشر",
+              IStrings.ActiveWindow => "النافذة الفعالة",
+
+              IStrings.SaveAs___ => "حفظ باسم...",
+              IStrings.FileAlreadyExists_ => "الملف موجود مسبقاً.",
+              IStrings.alreadyExists => "موجود مسبقاً",
+              IStrings.WouldYouLikeToReplaceTheExistingFile_ => "هل تودّ استبدال الملف السابق؟",
 
 
               //? Fallback to English.
