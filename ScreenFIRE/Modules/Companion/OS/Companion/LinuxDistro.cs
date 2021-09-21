7cba41767bc9bdb8a6ecf6494dc5fd3238ac134a
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-21 11:16:09 +0300

    move tests comments to `LinuxDistro.cs` & sorting elements

--- ScreenFIRE/Modules/Companion/OS/Companion/LinuxDistro.cs
+++ ScreenFIRE/Modules/Companion/OS/Companion/LinuxDistro.cs
@@ -3,20 +3,19 @@ using System;
 
 namespace ScreenFIRE.Modules.Companion.OS.Companion {
     public enum ILinuxDistro {
-        Debian,
-        SUSE,
+        AmazonLinux,
         Arch,
-
-        Ubuntu,
+        CentOS7,
+        Debian,
+        ElementaryOS,
+        Fedora,
         LinuxMint,
         OpenSUSE,
-        CentOS7,
-        Fedora,
-        ElementaryOS,
-        ZorinOS,
         OracleLinux,
         SparkyLinux,
-        AmazonLinux,
+        SUSE,
+        Ubuntu,
+        ZorinOS,
 
         Other
     }
@@ -24,23 +23,22 @@ namespace ScreenFIRE.Modules.Companion.OS.Companion {
     public class LinuxDistro {
         public static ILinuxDistro FromID(string linuxDistro)
             => linuxDistro.ToLower() switch {
-                "debian" => ILinuxDistro.Debian,
-                "suse" => ILinuxDistro.SUSE,
-                "arch" => ILinuxDistro.Arch,
-
-                "ubuntu" => ILinuxDistro.Ubuntu,
-                "linuxmint" => ILinuxDistro.LinuxMint,
-                "opensuse" => ILinuxDistro.OpenSUSE,
-                "suse opensuse" => ILinuxDistro.OpenSUSE,
-                "opensuse-leap" => ILinuxDistro.OpenSUSE,
-                "centos" => ILinuxDistro.CentOS7,
-                "fedora" => ILinuxDistro.Fedora,
-                "rhel fedora" => ILinuxDistro.Fedora,
-                "elementary" => ILinuxDistro.ElementaryOS,
-                "zorin" => ILinuxDistro.ZorinOS,
-                "ol" => ILinuxDistro.OracleLinux,
-                "sparky" => ILinuxDistro.SparkyLinux,
                 "amzn" => ILinuxDistro.AmazonLinux,
+                "arch" => ILinuxDistro.Arch,
+                "centos" => ILinuxDistro.CentOS7,
+                "debian" => ILinuxDistro.Debian,
+                "elementary" => ILinuxDistro.ElementaryOS,
+                "fedora" => ILinuxDistro.Fedora,
+                "linuxmint" => ILinuxDistro.LinuxMint,
+                "ol" => ILinuxDistro.OracleLinux,
+                "opensuse" => ILinuxDistro.OpenSUSE,
+                "opensuse-leap" => ILinuxDistro.OpenSUSE,
+                "rhel fedora" => ILinuxDistro.Fedora,
+                "sparky" => ILinuxDistro.SparkyLinux,
+                "suse opensuse" => ILinuxDistro.OpenSUSE,
+                "suse" => ILinuxDistro.SUSE,
+                "ubuntu" => ILinuxDistro.Ubuntu,
+                "zorin" => ILinuxDistro.ZorinOS,
 
                 _ => ILinuxDistro.Other
             };
@@ -49,50 +47,193 @@ namespace ScreenFIRE.Modules.Companion.OS.Companion {
         /// <summary> Information about the currently running Linux OS </summary>
         public record struct Current {
 
-            private static Bash _bash = null;
-            private static Bash bash => _bash ??= new();
+            private static Bash _Bash = null;
+            private static Bash Bash => _Bash ??= new();
 
 
             private static string ParseCommandString(string variable)
-                => ". /etc/*-release && echo " + variable;
+                => ". /etc/*-release && echo $" + variable;
 
             /// <summary> <c> >> /etc/*-release >> $ID </c> <br/>
             ///     ID of this distro <br/><br/>
             ///     # Example: ubuntu
             /// </summary>
             public static ILinuxDistro ID
-                => FromID(bash.Command(ParseCommandString("ID")).Output);
+                => FromID(Bash.Command(ParseCommandString("ID")).Output);
 
             /// <summary> <c> >> /etc/*-release >> $ID_LIKE </c> <br/>
             ///     ID of the distro this is based on <br/><br/>
             ///     # Example: debian (<see cref="string"/>)
             /// </summary>
             public static ILinuxDistro BaseID
-                => FromID(bash.Command(ParseCommandString("ID_LIKE")).Output);
+                => FromID(Bash.Command(ParseCommandString("ID_LIKE")).Output);
 
             /// <summary> <c> >> /etc/*-release >> $VERSION_ID </c> <br/>
             ///     Version presented in a numerical way <br/><br/>
             ///     # Example: 20.04 (<see cref="System.Version"/>) <br/>
             ///     <br/>
-            ///  !!!  Except Arch and CentOS 5~6 AND maybe others ¯\_(ツ)_/¯ 
+            ///  !!!  Except Arch and CentOS 5~6 AND maybe others ¯\_(ツ)_/¯
             /// </summary>
             public static Version Version
-                => Version.Parse(bash.Command(ParseCommandString("VERSION_ID")).Output);
+                => Version.Parse(Bash.Command(ParseCommandString("VERSION_ID")).Output);
 
             /// <summary> <c> >> /etc/*-release >> $PRETTY_NAME </c> <br/>
             ///     Name of this distro <br/><br/>
             ///     # Example: Ubuntu 20.04 LTS
             /// </summary>
             public static string Name
-                => bash.Command(ParseCommandString("PRETTY_NAME")).Output;
+                => Bash.Command(ParseCommandString("PRETTY_NAME")).Output;
 
             /// <summary> <c> >> /etc/*-release >> $HOME_URL </c> <br/>
             ///     Home page URL (<see cref="string"/>) of this distro <br/><br/>
             ///     # Example: https://www.ubuntu.com/ (<see cref="string"/>)
             /// </summary>
             public static string Homepage
-                => bash.Command(ParseCommandString("HOME_URL")).Output;
+                => Bash.Command(ParseCommandString("HOME_URL")).Output;
 
         }
     }
 }
+
+
+//!? TESTS
+//! >> $ID is common (All)
+//! >> $VERSION_ID is common (Except Arch || CentOS 6~5)
+
+//! >> Can be used
+//! $ . /etc/*-release && echo $name
+//? (output: value of name)
+
+//!? Ubuntu 20.04 ===================================
+//! $ cat /etc/*-release
+//  DISTRIB_ID=Ubuntu
+//  DISTRIB_RELEASE=20.04
+//  DISTRIB_CODENAME=focal
+//  DISTRIB_DESCRIPTION = "Ubuntu 20.04 LTS"
+//  NAME="Ubuntu"
+//  VERSION="20.04 LTS (Focal Fossa)"
+//  ID=ubuntu
+//  ID_LIKE = debian
+//  PRETTY_NAME="Ubuntu 20.04 LTS"
+//  VERSION_ID="20.04"
+//  HOME_URL="https://www.ubuntu.com/"
+//  SUPPORT_URL="https://help.ubuntu.com/"
+//  BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
+//  PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
+//  VERSION_CODENAME=focal
+//  UBUNTU_CODENAME = focal
+//!? ================================================
+
+//!? Arch v?? =======================================
+//! $ cat /etc/os-release
+//  NAME="Arch Linux"
+//  ID=arch
+//  PRETTY_NAME="Arch Linux"
+//  ANSI_COLOR="0;36"
+//  HOME_URL="https://www.archlinux.org/"
+//  SUPPORT_URL="https://bbs.archlinux.org/"
+//  BUG_REPORT_URL="https://bugs.archlinux.org/"
+//
+//? Not installed by default! `lsb-release`
+//! $ cat /etc/lsb-release
+//  LSB_VERSION=1.4-14
+//  DISTRIB_ID=Arch
+//  DISTRIB_RELEASE = rolling
+//  DISTRIB_DESCRIPTION="Arch Linux"
+//
+//? Empty: `/etc/arch-version`
+//!? ================================================
+
+//!? Amazon Linux 2016.09 ===========================
+//! $ cat /etc/os-release
+//  NAME="Amazon Linux AMI"
+//  VERSION="2016.09"
+//  ID="amzn"
+//  ID_LIKE="rhel fedora"
+//  VERSION_ID="2016.09"
+//  PRETTY_NAME="Amazon Linux AMI 2016.09"
+//  ANSI_COLOR="0;33"
+//  CPE_NAME="cpe:/o:amazon:linux:2016.09:ga"
+//  HOME_URL="http://aws.amazon.com/amazon-linux-ami/"
+//
+//? Empty: cat /etc/lsb-release
+//
+//! $ cat /etc/system-release
+//  Amazon Linux AMI release 2016.09
+//!? ================================================
+
+//!? CentOS 7 =======================================
+//! $ cat /etc/os-release
+//  NAME="CentOS Linux"
+//  VERSION="7 (Core)"
+//  ID="centos"
+//  ID_LIKE="rhel fedora"
+//  VERSION_ID="7"
+//  PRETTY_NAME="CentOS Linux 7 (Core)"
+//  ANSI_COLOR="0;31"
+//  CPE_NAME="cpe:/o:centos:centos:7"
+//  HOME_URL="https://www.centos.org/"
+//  BUG_REPORT_URL="https://bugs.centos.org/"
+//
+//  CENTOS_MANTISBT_PROJECT="CentOS-7"
+//  CENTOS_MANTISBT_PROJECT_VERSION="7"
+//  REDHAT_SUPPORT_PRODUCT="centos"
+//  REDHAT_SUPPORT_PRODUCT_VERSION="7"
+//
+//? Does not exist: /etc/lsb-release
+//!? ================================================
+
+//!? CentOS 6 and 5 =================================
+//? Does not exist: /etc/os-release
+//
+//! $ cat /etc/lsb-release
+//  LSB_VERSION=base-4.0-amd64:base-4.0-noarch:core-4.0-amd64:core-4.0-noarch
+//
+//! $ cat /etc/centos-release
+//  CentOS release 6.7 (Final)
+//!? ================================================
+
+//!? Fedora 22 ======================================
+//! $ cat  /etc/os-release
+//  NAME=Fedora
+//  VERSION="22 (Twenty Two)"
+//  ID=fedora
+//  VERSION_ID = 22
+//  PRETTY_NAME="Fedora 22 (Twenty Two)"
+//  ANSI_COLOR="0;34"
+//  CPE_NAME="cpe:/o:fedoraproject:fedora:22"
+//  HOME_URL="https://fedoraproject.org/"
+//  BUG_REPORT_URL="https://bugzilla.redhat.com/"
+//  REDHAT_BUGZILLA_PRODUCT="Fedora"
+//  REDHAT_BUGZILLA_PRODUCT_VERSION=22
+//  REDHAT_SUPPORT_PRODUCT="Fedora"
+//  REDHAT_SUPPORT_PRODUCT_VERSION=22
+//  PRIVACY_POLICY_URL=https://fedoraproject.org/wiki/Legal:PrivacyPolicy
+//
+//? Does not exist: /etc/lsb-release
+//
+//! $ cat /etc/fedora-release
+//  Fedora release 22 (Twenty Two)
+//!? ================================================
+
+//!? openSUSE Tumbleweed ============================
+//! $ cat  /etc/os-release
+//  NAME=openSUSE
+//  VERSION="20150725 (Tumbleweed)"
+//  VERSION_ID="20150725"
+//  PRETTY_NAME="openSUSE 20150725 (Tumbleweed) (x86_64)"
+//  ID=opensuse
+//  ANSI_COLOR = "0;32"
+//  CPE_NAME="cpe:/o:opensuse:opensuse:20150725"
+//  BUG_REPORT_URL="https://bugs.opensuse.org"
+//  HOME_URL="https://opensuse.org/"
+//  ID_LIKE="suse"
+//
+//? Does not exist: /etc/lsb-release
+//
+//! $ cat /etc/SuSE-release
+//  openSUSE 20150725 (x86_64)
+//  VERSION = 20150725
+//  CODENAME = Tumbleweed
+//  # /etc/SuSE-release is deprecated and will be removed in the future, use /etc/os-release instead
+//!? ================================================
--- ScreenFIRE/Modules/Companion/OS/Platform.cs
+++ ScreenFIRE/Modules/Companion/OS/Platform.cs
@@ -7,148 +7,6 @@ namespace ScreenFIRE.Modules.Companion.OS {
     /// <summary> Common class for general ScreenFIRE stuff </summary>
     public static class Platform {
 
-        //!? TESTS 
-        //! >> $ID is common (All)
-        //! >> $VERSION_ID is common (Except Arch || CentOS 6~5)
-
-        //! >> Can be used
-        //! $ . /etc/*-release && echo $name
-        //? (output: value of name)
-
-        //!? Ubuntu 20.04 ===================================
-        //! $ cat /etc/*-release
-        //  DISTRIB_ID=Ubuntu
-        //  DISTRIB_RELEASE=20.04
-        //  DISTRIB_CODENAME=focal
-        //  DISTRIB_DESCRIPTION = "Ubuntu 20.04 LTS"
-        //  NAME="Ubuntu"
-        //  VERSION="20.04 LTS (Focal Fossa)"
-        //  ID=ubuntu
-        //  ID_LIKE = debian
-        //  PRETTY_NAME="Ubuntu 20.04 LTS"
-        //  VERSION_ID="20.04"
-        //  HOME_URL="https://www.ubuntu.com/"
-        //  SUPPORT_URL="https://help.ubuntu.com/"
-        //  BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
-        //  PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
-        //  VERSION_CODENAME=focal
-        //  UBUNTU_CODENAME = focal 
-        //!? ================================================
-
-        //!? Arch v?? ======================================= 
-        //! $ cat /etc/os-release
-        //  NAME="Arch Linux"
-        //  ID=arch
-        //  PRETTY_NAME="Arch Linux"
-        //  ANSI_COLOR="0;36"
-        //  HOME_URL="https://www.archlinux.org/"
-        //  SUPPORT_URL="https://bbs.archlinux.org/"
-        //  BUG_REPORT_URL="https://bugs.archlinux.org/" 
-        //
-        //? Not installed by default! `lsb-release`
-        //! $ cat /etc/lsb-release
-        //  LSB_VERSION=1.4-14
-        //  DISTRIB_ID=Arch
-        //  DISTRIB_RELEASE = rolling
-        //  DISTRIB_DESCRIPTION="Arch Linux"
-        //
-        //? Empty: `/etc/arch-version`
-        //!? ================================================
-
-        //!? Amazon Linux 2016.09 =========================== 
-        //! $ cat /etc/os-release
-        //  NAME="Amazon Linux AMI"
-        //  VERSION="2016.09"
-        //  ID="amzn"
-        //  ID_LIKE="rhel fedora"
-        //  VERSION_ID="2016.09"
-        //  PRETTY_NAME="Amazon Linux AMI 2016.09"
-        //  ANSI_COLOR="0;33"
-        //  CPE_NAME="cpe:/o:amazon:linux:2016.09:ga"
-        //  HOME_URL="http://aws.amazon.com/amazon-linux-ami/"
-        //
-        //? Empty: cat /etc/lsb-release 
-        //
-        //! $ cat /etc/system-release
-        //  Amazon Linux AMI release 2016.09
-        //!? ================================================
-
-        //!? CentOS 7 ======================================= 
-        //! $ cat /etc/os-release
-        //  NAME="CentOS Linux"                                                                                                        
-        //  VERSION="7 (Core)"
-        //  ID="centos"
-        //  ID_LIKE="rhel fedora"
-        //  VERSION_ID="7"
-        //  PRETTY_NAME="CentOS Linux 7 (Core)"
-        //  ANSI_COLOR="0;31"
-        //  CPE_NAME="cpe:/o:centos:centos:7"
-        //  HOME_URL="https://www.centos.org/"
-        //  BUG_REPORT_URL="https://bugs.centos.org/"
-        //  
-        //  CENTOS_MANTISBT_PROJECT="CentOS-7"
-        //  CENTOS_MANTISBT_PROJECT_VERSION="7"
-        //  REDHAT_SUPPORT_PRODUCT="centos"
-        //  REDHAT_SUPPORT_PRODUCT_VERSION="7" 
-        //
-        //? Does not exist: /etc/lsb-release
-        //!? ================================================
-
-        //!? CentOS 6 and 5 =================================
-        //? Does not exist: /etc/os-release  
-        //
-        //! $ cat /etc/lsb-release
-        //  LSB_VERSION=base-4.0-amd64:base-4.0-noarch:core-4.0-amd64:core-4.0-noarch 
-        //
-        //! $ cat /etc/centos-release
-        //  CentOS release 6.7 (Final)
-        //!? ================================================
-
-        //!? Fedora 22 ======================================
-        //! $ cat  /etc/os-release  
-        //  NAME=Fedora
-        //  VERSION="22 (Twenty Two)"
-        //  ID=fedora
-        //  VERSION_ID = 22
-        //  PRETTY_NAME="Fedora 22 (Twenty Two)"
-        //  ANSI_COLOR="0;34"   
-        //  CPE_NAME="cpe:/o:fedoraproject:fedora:22"
-        //  HOME_URL="https://fedoraproject.org/"
-        //  BUG_REPORT_URL="https://bugzilla.redhat.com/"
-        //  REDHAT_BUGZILLA_PRODUCT="Fedora"
-        //  REDHAT_BUGZILLA_PRODUCT_VERSION=22
-        //  REDHAT_SUPPORT_PRODUCT="Fedora"
-        //  REDHAT_SUPPORT_PRODUCT_VERSION=22
-        //  PRIVACY_POLICY_URL=https://fedoraproject.org/wiki/Legal:PrivacyPolicy
-        //
-        //? Does not exist: /etc/lsb-release 
-        //
-        //! $ cat /etc/fedora-release
-        //  Fedora release 22 (Twenty Two)
-        //!? ================================================
-
-        //!? openSUSE Tumbleweed ============================
-        //! $ cat  /etc/os-release  
-        //  NAME=openSUSE
-        //  VERSION="20150725 (Tumbleweed)"
-        //  VERSION_ID="20150725"
-        //  PRETTY_NAME="openSUSE 20150725 (Tumbleweed) (x86_64)"
-        //  ID=opensuse
-        //  ANSI_COLOR = "0;32"
-        //  CPE_NAME="cpe:/o:opensuse:opensuse:20150725"
-        //  BUG_REPORT_URL="https://bugs.opensuse.org"
-        //  HOME_URL="https://opensuse.org/"
-        //  ID_LIKE="suse"
-        //
-        //? Does not exist: /etc/lsb-release 
-        //
-        //! $ cat /etc/SuSE-release
-        //  openSUSE 20150725 (x86_64)
-        //  VERSION = 20150725
-        //  CODENAME = Tumbleweed
-        //  # /etc/SuSE-release is deprecated and will be removed in the future, use /etc/os-release instead
-        //!? ================================================
-
 
 
         /// <summary> Indicate if the current platform is Unix based </summary>
@@ -157,17 +15,13 @@ namespace ScreenFIRE.Modules.Companion.OS {
 
 
 
-
-
-
         /// <summary> Indicate if the current platform is Win32NT based (Windows NT and above) </summary>
         public static bool RunningWindows
                 => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-
         /// <summary> Indicates if Windows 10 (10.0...) is the current OS</summary>
         public static bool RunningWindows10
             => RunningWindows
-                && //! Step into only if running Windows 
+                && //! Step into only if running Windows
                     (Environment.OSVersion.Version.ToString(2)
                         == IWindowsVersion.Windows10.ToString(2));
 
