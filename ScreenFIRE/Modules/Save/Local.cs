17690eec94fa82a389436685b41516bf28a3bdd3
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-21 16:16:07 +0300

    just some spaces

--- ScreenFIRE/Modules/Save/Local.cs
+++ ScreenFIRE/Modules/Save/Local.cs
@@ -9,128 +9,128 @@ using System.Threading.Tasks;
 namespace ScreenFIRE.Modules.Save {
 
 
-    partial class Save {
+	partial class Save {
 
-        private static string[] txt_privatenameusedonlybythisfunction_028357023857203109841234 = null;
-        private static async Task<string> txt(int index) {
-            return (txt_privatenameusedonlybythisfunction_028357023857203109841234
-                   //! Set while fetching
-                   ??= await Strings.Fetch(IStrings.SaveAs___, /*0*/
-                                            IStrings.OK, /*1*/
-                                            IStrings.Yes, /*2*/
-                                            IStrings.No, /*3*/
-                                            IStrings.Cancel, /*4*/
-                                            IStrings.FileAlreadyExists_, /*5*/
-                                            IStrings.WouldYouLikeToReplaceTheExistingFile_ /*6*/
-                                            )
-                        )[index];
-        }
+		private static string[] txt_privatenameusedonlybythisfunction_028357023857203109841234 = null;
+		private static async Task<string> txt(int index) {
+			return (txt_privatenameusedonlybythisfunction_028357023857203109841234
+				   //! Set while fetching
+				   ??= await Strings.Fetch(IStrings.SaveAs___, /*0*/
+											IStrings.OK, /*1*/
+											IStrings.Yes, /*2*/
+											IStrings.No, /*3*/
+											IStrings.Cancel, /*4*/
+											IStrings.FileAlreadyExists_, /*5*/
+											IStrings.WouldYouLikeToReplaceTheExistingFile_ /*6*/
+											)
+						)[index];
+		}
 
-        /// <summary> ••• GUI ••• Save a <see cref="Screenshot"/> locally </summary>
-        public static async Task<bool> Local(Screenshot screenshot, Window parentWindow) {
+		/// <summary> ••• GUI ••• Save a <see cref="Screenshot"/> locally </summary>
+		public static async Task<bool> Local(Screenshot screenshot, Window parentWindow) {
 
-            //! Parameters to be passed
-            //Screenshot screenshot;
-            string path;
-            bool replaceExisting = false;
-            ISaveFormat saveFormat = ISaveFormat.png;
-            ///////////////////////////
+			//! Parameters to be passed
+			//Screenshot screenshot;
+			string path;
+			bool replaceExisting = false;
+			ISaveFormat saveFormat = ISaveFormat.png;
+			///////////////////////////
 
-            //! Let the user choose a path to the file
-            FileChooserDialog choose
-                    = new(await txt(0),
-                          parentWindow,
-                          FileChooserAction.Save);
-            choose.SelectMultiple = false;
-            choose.AddButton(Stock.Ok, ResponseType.Ok);
-            choose.AddButton(Stock.Cancel, ResponseType.Cancel);
-            choose.SetCurrentFolder(Common.SF);
-            choose.SetPosition(WindowPosition.CenterOnParent);
+			//! Let the user choose a path to the file
+			FileChooserDialog choose
+					= new(await txt(0),
+						  parentWindow,
+						  FileChooserAction.Save);
+			choose.SelectMultiple = false;
+			choose.AddButton(Stock.Ok, ResponseType.Ok);
+			choose.AddButton(Stock.Cancel, ResponseType.Cancel);
+			choose.SetCurrentFolder(Common.SF);
+			choose.SetPosition(WindowPosition.CenterOnParent);
 
-            ResponseType chooseResponse = (ResponseType)choose.Run();
+			ResponseType chooseResponse = (ResponseType)choose.Run();
 
-            //! User closed the dialog
-            if (chooseResponse == ResponseType.Ok) {
+			//! User closed the dialog
+			if (chooseResponse == ResponseType.Ok) {
 
-                path = choose.Filename;
+				path = choose.Filename;
 
-                //! User chose a file that already exists
-                if (choose.File.Exists) {
-                    //! Warn about replacing the file
-                    MessageDialog fileExistsDialog
-                            = new(parentWindow,
-                                  DialogFlags.DestroyWithParent,
-                                  MessageType.Question,
-                                  ButtonsType.YesNo,
-                                  await txt(5) + Common.nn + await txt(6));
-                    fileExistsDialog.SetPosition(WindowPosition.CenterOnParent);
-                    ResponseType fileExistsResponse = (ResponseType)fileExistsDialog.Run();
-                    //! User chose to replace the file
-                    if (fileExistsResponse == ResponseType.Yes)
-                        replaceExisting = true;
+				//! User chose a file that already exists
+				if (choose.File.Exists) {
+					//! Warn about replacing the file
+					MessageDialog fileExistsDialog
+							= new(parentWindow,
+								  DialogFlags.DestroyWithParent,
+								  MessageType.Question,
+								  ButtonsType.YesNo,
+								  await txt(5) + Common.nn + await txt(6));
+					fileExistsDialog.SetPosition(WindowPosition.CenterOnParent);
+					ResponseType fileExistsResponse = (ResponseType)fileExistsDialog.Run();
+					//! User chose to replace the file
+					if (fileExistsResponse == ResponseType.Yes)
+						replaceExisting = true;
 
-                    fileExistsDialog.Destroy();
-                }
+					fileExistsDialog.Destroy();
+				}
 
-            } else { //! User cancelled the screenshot saving operation 
-                choose.Destroy();
-                return false;
-            }
-            choose.Destroy();
+			} else { //! User cancelled the screenshot saving operation
+				choose.Destroy();
+				return false;
+			}
+			choose.Destroy();
 
-            //! Pass to ••• Specific ••• with the info provided by the user
-            return Local(screenshot, path, replaceExisting, saveFormat);
-        }
-
-
-        /// <summary> ••• AUTO ••• Save a <see cref="Screenshot"/> locally </summary>
-        public static bool Local(Screenshot screenshot, ISaveFormat saveFormat = ISaveFormat.png) {
-            //! Pass to ••• Specific ••• but with auto generated info
-            return Local(screenshot,
-                         path: Path.Combine(Common.SF,
-                                            Strings.Fetch(IStrings.ScreenFIRE)
-                                            + $"_{screenshot.Time:yyMMdd-HHmmff}"
-                                            + $".{saveFormat}"),
-                         replaceExisting: false,
-                         saveFormat: ISaveFormat.png);
-        }
+			//! Pass to ••• Specific ••• with the info provided by the user
+			return Local(screenshot, path, replaceExisting, saveFormat);
+		}
 
 
-        /// <summary> ••• Specific ••• Save a <see cref="Screenshot"/> locally </summary>
-        /// <returns> false if cancelled of failed to save</returns>
-        public static bool Local(Screenshot screenshot,
-                                 string path,
-                                 bool replaceExisting = false,
-                                 ISaveFormat saveFormat = ISaveFormat.png) {
+		/// <summary> ••• AUTO ••• Save a <see cref="Screenshot"/> locally </summary>
+		public static bool Local(Screenshot screenshot, ISaveFormat saveFormat = ISaveFormat.png) {
+			//! Pass to ••• Specific ••• but with auto generated info
+			return Local(screenshot,
+						 path: Path.Combine(Common.SF,
+											Strings.Fetch(IStrings.ScreenFIRE)
+											+ $"_{screenshot.Time:yyMMdd-HHmmff}"
+											+ $".{saveFormat}"),
+						 replaceExisting: false,
+						 saveFormat: ISaveFormat.png);
+		}
 
-            try {//! Try to use provided path
 
-                //! Deal with file replacement
-                if (File.Exists(path) & !replaceExisting) {
-                    path = Path.Combine(
-                                Path.GetDirectoryName(path),
-                                Path.GetFileNameWithoutExtension(path)
-                                + $"_{screenshot.Time:yyMMdd-HHmmff}"
-                                + $".{saveFormat}");
-                }
+		/// <summary> ••• Specific ••• Save a <see cref="Screenshot"/> locally </summary>
+		/// <returns> false if cancelled of failed to save</returns>
+		public static bool Local(Screenshot screenshot,
+								 string path,
+								 bool replaceExisting = false,
+								 ISaveFormat saveFormat = ISaveFormat.png) {
 
-                //! Make sure the extension is added
-                if (string.IsNullOrEmpty(Path.GetExtension(path)))
-                    path += $".{saveFormat}";
+			try {//! Try to use provided path
 
-                //! Save
-                screenshot.Image.Save(path, saveFormat.ToString());
-                return true;
+				//! Deal with file replacement
+				if (File.Exists(path) & !replaceExisting) {
+					path = Path.Combine(
+								Path.GetDirectoryName(path),
+								Path.GetFileNameWithoutExtension(path)
+								+ $"_{screenshot.Time:yyMMdd-HHmmff}"
+								+ $".{saveFormat}");
+				}
 
-            } catch {
+				//! Make sure the extension is added
+				if (string.IsNullOrEmpty(Path.GetExtension(path)))
+					path += $".{saveFormat}";
 
-                try { //! Try to use default path
+				//! Save
+				screenshot.Image.Save(path, saveFormat.ToString());
+				return true;
 
-                    return Local(screenshot, saveFormat); //! (Auto)  
+			} catch {
 
-                } catch { return false; } //! Something went wrong
+				try { //! Try to use default path
 
-            }
-        }
-    }
+					return Local(screenshot, saveFormat); //! (Auto)
+
+				} catch { return false; } //! Something went wrong
+
+			}
+		}
+	}
 }
