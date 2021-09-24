56e330c5aad06b2b1426e7ed6f5cca65cc52cc64
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-24 23:37:18 +0300

    add gnome guidelines-specific strings

--- ScreenFIRE/Modules/Companion/Common.cs
+++ ScreenFIRE/Modules/Companion/Common.cs
@@ -3,8 +3,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 
-using env = System.Environment;
-
 
 namespace ScreenFIRE.Modules.Companion {
 
@@ -12,48 +10,80 @@ namespace ScreenFIRE.Modules.Companion {
     static class Common {
 
         #region ScreenFIRE environment
+        /// <summary> (<see cref="Environment.SpecialFolder.UserProfile"/>) </summary>
+        public static string UserProfile => Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 
-        /// <summary> (<see cref="env.SpecialFolder.MyPictures"/>) > ScreenFIRE </summary>
+        /// <summary> (<see cref="Environment.SpecialFolder.MyPictures"/>) > ScreenFIRE </summary>
         public static string SF
-            => Path.Combine(env.GetFolderPath(env.SpecialFolder
+            => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder
                                 .MyPictures),
                             "ScreenFIRE");
 
         /// <summary> (<see cref="env.SpecialFolder.ApplicationData"/>) > ScreenFIRE </summary>
         public static string SF_Data
-            => Path.Combine(env.GetFolderPath(env.SpecialFolder
+            => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder
                                 .ApplicationData),
                             "ScreenFIRE");
 
         #endregion
 
 
-        #region Public Abbreviations 
+        #region Public Abbreviations
 
-        /// <summary> Get current copyright string </summary>
+        /// <returns> Current copyright string </returns>
         public static string Copyright => FileVersionInfo.GetVersionInfo(
                                                 Assembly.GetExecutingAssembly().Location
                                           ).LegalCopyright;
 
-        /// <summary> Get current version field </summary>
+        /// <returns> Get current version field </summary>
         public static Version Version => Assembly.GetExecutingAssembly().GetName().Version;
 
-        /// <summary> Simple .NET new line (<see cref="env.NewLine"/>) <br/>
-        /// \r\n for non-Unix platforms, or \n for Unix platforms. </summary>
-        public static string n => env.NewLine;
-        /// <summary> Double! .NET new line (<see cref="env.NewLine"/>) <br/>
-        /// \r\n\r\n for non-Unix platforms, or \n\n for Unix platforms. </summary>
-        public static string nn => n + n;
-        /// <summary> Simple html new line (&lt;br/&gt;) </summary>
-        public static string br => "<br/>";
-        /// <summary> Double! Simple html new line (&lt;br/&gt;) </summary>
-        public static string brbr => br + br;
-        /// <summary> Tab: (Unicode) U+3000 | (HTML) And#12288; | (Description) Ideographic Space </summary>
-        public static string tab => @"　";
-        /// <summary> Double! Tab: (Unicode) U+3000 | (HTML) And#12288; | (Description) Ideographic Space </summary>
-        public static string tabtab => tab + tab;
+        /// <returns> Simple .NET new line (<see cref="Environment.NewLine"/>) <br/>
+        /// \r\n for non-Unix platforms, or \n for Unix platforms. </returns>
+        public static string n = Environment.NewLine;
+        /// <summary> Double! .NET new line (<see cref="Environment.NewLine"/>) <br/>
+        /// \r\n\r\n for non-Unix platforms, or \n\n for Unix platforms. </returns>
+        public static string nn = n + n;
 
-        #endregion 
+        /// <summary> Ideographic Space (Tab)</summary>
+        /// <returns> ( <c>\u3000</c> )</returns>
+        public const string tab = "\u3000";
+
+        ///<summary> Narrow no-break space </summary>
+        /// <returns> (ex: 32GB -> 32 GB ) ( <c>\u202F</c> )</returns>
+        public const string NarrowSpace = "\u202F";
+
+        ///<summary> Horizontal Ellipses </summary>
+        /// <returns> (3 dots) ( … ) ( <c>\u2026</c> )</returns>
+        public const string Ellipses = @"…"; //"\u2026";
+        ///<summary> Vertical Ellipses </summary>
+        /// <returns> (Vertical 3 dots) ( ︙ ) ( <c>\uFE19</c> )</returns>
+        public const string Ellipses_Vertical = @"︙"; //"\uFE19";
+
+        ///<summary> Right single quotation mark </summary>
+        /// <returns> ( ’ ) ( <c>\u2019</c> )</returns>
+        public const string Apostrophe = @"’"; //"\u2019";
+
+        ///<summary> Curved left double quotation </summary>
+        /// <returns> ( “ ) ( <c>\u201C</c> )</returns>
+        public const string DoubleQuotation_Left = @"“"; //"\u201C";
+        ///<summary> Curved right double quotation </summary>
+        /// <returns> ( ” ) ( <c>\u201D</c> )</returns>
+        public const string DoubleQuotation_Right = @"”"; //"\u201D";
+
+        ///<summary> Ratio symbol </summary>
+        /// <returns> ( ∶ ) ( <c>\u2236</c> )</returns>
+        public const string Ratio = @"∶"; //"\u2236";
+
+        ///<summary> Bullet symbol </summary>
+        /// <returns> ( • ) ( <c>\u2022</c> )</returns>
+        public const string Bullet = @"•"; //"\u2022";
+
+        ///<summary> En dash </summary>
+        /// <returns> ( – ) ( <c>\u2013</c> ) </returns>
+        public const string RangeDash = @"–"; //"\u2013";
+
+        #endregion
 
     }
 }
