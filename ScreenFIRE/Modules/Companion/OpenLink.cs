05ceb58853a2a4d6f0962e7d0564a9e2acb3c227
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-25 22:59:03 +0300

    init (Open links on .net core)

---
+++ ScreenFIRE/Modules/Companion/OpenLink.cs
@@ -0,0 +1,24 @@
+﻿using ScreenFIRE.Modules.Companion.OS;
+using System.Diagnostics;
+
+namespace ScreenFIRE.Modules.Companion {
+    public static class OpenLink {
+        public static bool Run(string url) {
+            try {
+                Process.Start(url);
+            } catch {
+                if (Platform.RunningWindows)
+                    Process.Start(
+                        new ProcessStartInfo("cmd", $"/c start {url.Replace("&", "^&")}") {
+                            CreateNoWindow = true
+                        });
+
+                else if (Platform.RunningLinux)
+                    Process.Start("xdg-open", url);
+
+                else return false;
+            }
+            return true;
+        }
+    }
+}
