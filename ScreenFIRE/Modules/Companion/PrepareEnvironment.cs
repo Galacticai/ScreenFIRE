9544c2a5d8d27a3407f8337c18ff7f952daea156
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-27 16:24:22 +0300

    add `SaveOptions` init section & add descriptions (comments)

--- ScreenFIRE/Modules/Companion/PrepareEnvironment.cs
+++ ScreenFIRE/Modules/Companion/PrepareEnvironment.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using env = System.Environment;
+﻿using env = System.Environment;
 
 namespace ScreenFIRE.Modules.Companion {
 
@@ -7,18 +6,26 @@ namespace ScreenFIRE.Modules.Companion {
     internal static class PrepareEnvironment {
         public static bool Run() {
 
+            //? User profile is missing
             if (!Directory.Exists(Common.UserProfile))
-                return false; //! User profile is missing
+                return false;
 
+            //! User >> Pictures
             if (!Directory.Exists(env.GetFolderPath(env.SpecialFolder.MyPictures)))
                 Directory.CreateDirectory(
                             env.GetFolderPath(env.SpecialFolder.MyPictures));
 
+            //! (User) >> Pictures >> ScreenFIRE
             if (!Directory.Exists(Common.SF))
                 Directory.CreateDirectory(Common.SF);
 
+            //! (System App Data) >> ScreenFIRE
             if (!Directory.Exists(Common.SF_Data))
                 Directory.CreateDirectory(Common.SF_Data);
+
+            //! SaveOptions
+            if (string.IsNullOrEmpty(Common.SaveOptions.Location))
+                Common.SaveOptions.Location = Common.SF;
 
             return true;
         }
