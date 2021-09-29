ecd74f4a162cba48e3b68e9d4a8a1b117d7e138e
NH Komaiha <nhkomaiha@hotmail.com>
2021-09-29 21:25:45 +0300

    update

---
+++ ScreenFIRE/Modules/Companion/PathIsRW.cs
@@ -0,0 +1,13 @@
+﻿namespace ScreenFIRE.Modules.Companion {
+    class PathIsRW {
+        public static bool Run(string path) {
+            try {
+                string lockfile = Path.Combine(path, "lock.file");
+                using var fs = File.Create(lockfile, bufferSize: 1, FileOptions.DeleteOnClose);
+                fs.Close();
+                File.Delete(lockfile); //idk just doubling down i guess
+                return true;
+            } catch { return false; }
+        }
+    }
+}
