402eba3564b930b96cc3619f824779bf5179999f
NH Komaiha <nhkomaiha@hotmail.com>
2021-10-03 19:16:01 +0300

    init Clipboard

--- ScreenFIRE/Modules/Save/Clipboard.cs
+++ ScreenFIRE/Modules/Save/Clipboard.cs
@@ -1,6 +1,13 @@
-﻿namespace ScreenFIRE.Modules.Save {
+﻿using ScreenFIRE.Modules.Capture;
+
+namespace ScreenFIRE.Modules.Save {
 
     partial class Save {
-
+        public static bool Clipboard(Screenshot screenshot) {
+            try {
+                Gtk.Clipboard.GetDefault(Gdk.Display.Default).Image = screenshot.Image;
+                return true;
+            } catch { return false; }
+        }
     }
 }
