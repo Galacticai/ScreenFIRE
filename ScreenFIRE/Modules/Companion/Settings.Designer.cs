8adb6d268e155c7d94f0f9df663cfb9d77f6a422
xeroling <nhkomaiha@hotmail.com>
2021-10-08 19:17:16 +0300

    fix missing default value for `LastLanguage`

--- ScreenFIRE/Modules/Companion/Settings.Designer.cs
+++ ScreenFIRE/Modules/Companion/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ScreenFIRE.Modules.Companion {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.2.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,6 +25,7 @@ namespace ScreenFIRE.Modules.Companion {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("System")]
         public global::ScreenFIRE.Modules.Companion.ILanguages LastLanguage {
             get {
                 return ((global::ScreenFIRE.Modules.Companion.ILanguages)(this["LastLanguage"]));
