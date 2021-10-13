daaf64999fbd486991687d4986a4f7c70ec3dcab
xeroling <nhkomaiha@hotmail.com>
2021-10-13 16:32:48 +0300

    update names & xaml docs

--- ScreenFIRE/Modules/Companion/math/Functions.cs
+++ ScreenFIRE/Modules/Companion/math/Functions.cs
@@ -12,19 +12,19 @@ namespace ScreenFIRE.Modules.Companion.math {
 
         /// <summary>
         /// <list>
-        /// <item>Puts the value on a curve function</item>
+        /// <item> Plug <paramref name="x"/> into the following function: </item>
         /// <c>
-        /// <item>‏‏‎s‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎ ‏‏‎ ‎‎‏‏‎ ‎.-  </item>
-        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎ ‎/    </item>
-        /// <item>‏‏‎: ‎_-     </item>
+        /// <item>‏‏‎s‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎ ‏‏‎ ‎‎‏‏‎ ‎.-   </item>
+        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎ ‎/      </item>
+        /// <item>‏‏‎: ‎_-        </item>
         /// <item>‏‏‎‎0 . . . . s </item>
         /// </c>
         /// </list>
         /// </summary>
         /// <param name="x">input</param>
         /// <param name="scale">Size of the wave (Half wave)</param>
-        /// <returns>Returns <c>f(x)</c></returns>
-        public static double cosSmoothStartEnd(double x, double scale) {
+        /// <returns> <c>f(<paramref name="x"/>)</c> </returns>
+        public static double SmoothStartEnd_Increasing(double x, double scale) {
             mathMisc.ForcedInRange(ref x, 0, scale); // force x between 0一一一s
             return scale * (-Math.Cos(x * Math.PI) / (2 * scale) + 1 / 2);
         }
@@ -37,19 +37,19 @@ namespace ScreenFIRE.Modules.Companion.math {
 
         /// <summary>
         /// <list>
-        /// <item>Puts the value on a curve function</item>
+        /// <item> Plug <paramref name="x"/> into the following function: </item>
         /// <c>
-        /// <item>‏‏‎‎s‏‏‎ ‎‎-.      </item>
-        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\    </item>
-        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎-_</item>
+        /// <item>‏‏‎‎s‏‏‎ ‎‎-.        </item>
+        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\      </item>
+        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎-_   </item>
         /// <item>‏‏‎0 . . . . s </item>
         /// </c>
         /// </list>
         /// </summary>
         /// <param name="x">input</param>
         /// <param name="scale">Size of the wave (Half wave)</param>
-        /// <returns>Returns <c>f(x)</c></returns>
-        public static double sinSmoothEnd_01(double x, double scale) {
+        /// <returns> <c>f(<paramref name="x"/>)</c> </returns>
+        public static double SmoothStartEnd_Decreasing(double x, double scale) {
             mathMisc.ForcedInRange(ref x, 0, scale); // force x between 0一一一s
             return scale * (Math.Sin(x * Math.PI + Math.PI / 2) / 2 + 1 / 2);
         }
@@ -62,19 +62,19 @@ namespace ScreenFIRE.Modules.Companion.math {
 
         /// <summary>
         /// <list>
-        /// <item>Puts the value on a curve function</item>
+        /// <item> Plug <paramref name="x"/> into the following function: </item>
         /// <c>
-        /// <item>‏‏‎‎s‏‏‎ ‎\</item>
-        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\</item>
-        /// <item>‏‏‎:‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎*._</item>
+        /// <item>‏‏‎‎s‏‏‎ ‎\         </item>
+        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\       </item>
+        /// <item>‏‏‎:‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎*._   </item>
         /// <item>‏‏‎0 . . . . s </item>
         /// </c>
         /// </list>
         /// </summary>
         /// <param name="x">input</param>
         /// <param name="scale">Size of the wave (Half wave)</param>
-        /// <returns>Returns <c>f(x)</c></returns>
-        public static double sinSmoothEnd(double x, double scale) {
+        /// <returns> <c>f(<paramref name="x"/>)</c> </returns>
+        public static double SmoothEnd_Decreasing(double x, double scale) {
             mathMisc.ForcedInRange(ref x, 0, scale); // force x between 0一一一s
             return scale * (-Math.Sin(x * (Math.PI / 2) / scale) + 1);
         }
@@ -87,19 +87,19 @@ namespace ScreenFIRE.Modules.Companion.math {
 
         /// <summary>
         /// <list>
-        /// <item>Puts the value on a curve function</item>
+        /// <item> Plug <paramref name="x"/> into the following function: </item>
         /// <c>
-        /// <item>‏‏‎‎s‏‏‎‏‏‎ ‎--.</item>
-        /// <item>‏‏‎:‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\</item>
-        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\</item>
+        /// <item>‏‏‎‎s‏‏‎‏‏‎ ‎--.       </item>
+        /// <item>‏‏‎:‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\     </item>
+        /// <item>‏‏‎:‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\   </item>
         /// <item>‏‏‎0 . . . . s </item>
         /// </c>
         /// </list>
         /// </summary>
         /// <param name="x">input</param>
         /// <param name="scale">Size of the wave (Half wave)</param>
-        /// <returns>Returns <c>f(x)</c></returns>
-        public static double sinSmoothStart(double x, double scale) {
+        /// <returns> <c>f(<paramref name="x"/>)</c> </returns>
+        public static double SmoothStart_Decreasing(double x, double scale) {
             mathMisc.ForcedInRange(ref x, 0, scale); // force x between 0一一一s
             return scale * Math.Sin(x * (Math.PI / (2 * scale)) + Math.PI / 2);
         }
