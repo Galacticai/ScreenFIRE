83bd8c82552e9f12c893bc62418841742c7d202c
xeroling <nhkomaiha@hotmail.com>
2021-10-22 15:50:21 +0300

    placeholder: add IsDarkTheme bool

--- ScreenFIRE/Modules/Companion/Theme.cs
+++ ScreenFIRE/Modules/Companion/Theme.cs
@@ -1,5 +1,6 @@
 ﻿namespace ScreenFIRE.Modules.Companion {
     internal class Theme {
+        //public static bool IsDarkTheme => Application.Current.RequestedTheme == ApplicationTheme.Dark;
         public static void placeholder() {
             Gtk.CssProvider css_provider = new();
             css_provider.LoadFromData("themes/DeLorean-Dark-3.14/gtk-3.0/gtk.css");
