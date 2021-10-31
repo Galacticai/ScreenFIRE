b7a1715ecb90229a245543d41b80e320c2e10f19
xeroling <nhkomaiha@hotmail.com>
2021-10-31 12:58:51 +0200

    rename `AddArrays` to `Add`

--- ScreenFIRE/Modules/Companion/math/Arrays.cs
+++ ScreenFIRE/Modules/Companion/math/Arrays.cs
@@ -7,9 +7,9 @@ namespace ScreenFIRE.Modules.Companion.math {
         /// <summary> Add <paramref name="elements"/> array to the end of <paramref name="array"/></summary>
         /// <typeparam name="T">Type of the array to be used</typeparam>
         /// <param name="array">Array to manipulate</param>
-        /// <param name="expansion">Element to add to <paramref name="array"/></param>
+        /// <param name="elements">Element to add to <paramref name="array"/></param>
         /// <returns>Array {<paramref name="array"/>, <paramref name="expansion"/>} as <typeparamref name="T"/>[]</returns>
-        public static T[] AddArrays<T>(this T[] array, params T[] elements)
+        public static T[] Add<T>(this T[] array, params T[] elements)
             => array.Concat(elements).ToArray();
     }
 }
