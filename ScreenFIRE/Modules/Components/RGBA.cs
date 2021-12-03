61f029e0b20aff6551e77635d7aabe0c7a4b3dd6
xeroling <nhkomaiha@hotmail.com>
2021-12-03 23:26:56 +0200

    init (static method for `Gdk.RGBA`)

---
+++ ScreenFIRE/Modules/Components/RGBA.cs
@@ -0,0 +1,10 @@
+﻿using g = Gdk;
+
+namespace ScreenFIRE.Modules.Components {
+
+    public static class RGBA {
+        public static g.RGBA New(double R, double G, double B, double A) {
+            return new() { Red = R, Green = G, Blue = B, Alpha = A };
+        }
+    }
+}
