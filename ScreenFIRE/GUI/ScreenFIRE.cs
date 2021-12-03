6c372514c9e3985c3759e838ad890a5b97573618
xeroling <nhkomaiha@hotmail.com>
2021-12-03 20:19:46 +0200

    add the foundation for area selection and toolbox placement

--- ScreenFIRE/GUI/ScreenFIRE.cs
+++ ScreenFIRE/GUI/ScreenFIRE.cs
@@ -1,8 +1,10 @@
 using Gtk;
 using ScreenFIRE.Modules.Capture;
 using ScreenFIRE.Modules.Capture.Companion;
-using ScreenFIRE.Modules.Companion;
+using ScreenFIRE.Modules.Companion.math;
+using ScreenFIRE.Modules.Companion.math.Vision;
 using ScreenFIRE.Modules.Companion.math.Vision.Geometry;
+using ScreenFIRE.Modules.Components.DrawObject;
 using System;
 using System.Threading;
 using c = Cairo;
@@ -15,8 +17,10 @@ namespace ScreenFIRE.GUI {
 
         private Screenshot Screenshot { get; set; }
 
+        private DrawType _drawType = DrawType.None;
+        internal DrawType drawType { get => _drawType; set => _drawType = value; }
+
         [UI] private readonly Image ScreenshotImage = null;
-        [UI] private readonly Button Close_Button = null;
         [UI] private readonly DrawingArea SSDrawingArea = null;
 
         private void AssignEvents() {
@@ -32,12 +36,17 @@ namespace ScreenFIRE.GUI {
             ButtonReleaseEvent += Draw_ButtonReleaseEvent;
             SSDrawingArea.Drawn += new DrawnHandler(SSDrawingArea_Drawn);
         }
+        private void AssignEtc() {
+            KeepAbove = true;
+            Decorated = false;
+        }
 
         public ScreenFIRE() : this(new Builder("ScreenFIRE.glade")) { }
 
         private ScreenFIRE(Builder builder) : base(builder.GetRawOwnedObject("ScreenFIRE")) {
             builder.Autoconnect(this);
             AssignEvents();
+            AssignEtc();
         }
 
         private void OnHidden(object sender, EventArgs ev) {
@@ -45,8 +54,9 @@ namespace ScreenFIRE.GUI {
             Program.Config.ShowAll();
         }
         private void OnShown(object sender, EventArgs ev) {
-            var displaySize = Monitors.BoundingRectangle();
-            Move(displaySize.Width, displaySize.Height);
+            //var displaySize = Monitors.BoundingRectangle();
+            //Move(displaySize.Width, displaySize.Height);
+            //Boundary = new(0, 0, 0, 0);
 
             Program.Config.Hide();
 
@@ -61,78 +71,126 @@ namespace ScreenFIRE.GUI {
             SSDrawingArea.SetSizeRequest(Screenshot.ImageRectangle.Width,
                                          Screenshot.ImageRectangle.Height);
         }
-
-        private bool DRAWING = false;
-        private c.PointD startPoint = new(0, 0);
+        private c.PointD startPoint;// = new(0, 0);
         private c.PointD endPoint;
-        private c.Rectangle Boundary;
-        private g.Pixbuf lastFrame;
+        internal c.Rectangle Boundary;
+        private double BoundaryAlpha = 1;
+        private bool BoundaryAlphaIncreasing = true;
+        private double dimAlpha = 0;
+        private double dashesOffset = 5;
         private void SSDrawingArea_Drawn(object sender, DrawnArgs ev) {
+
             using c.Context context = ev.Cr;
             context.Antialias = c.Antialias.Subpixel;
 
-            context.LineCap = c.LineCap.Round;
-            context.LineWidth = 5;
-
-            context.X_Sign(Boundary);
-            context.Stroke();
-
-            context.SetSourceRGB(.6, .3, .4);
-            context.RoundedRectangle(Boundary);
-            context.Stroke();
-            context.SetSourceRGBA(.6, .3, .5, .5);
+            c.Rectangle ssRect = Screenshot.ImageRectangle.ToCairoRectangle();
+            if (dimAlpha < .5) dimAlpha += .05;
+            context.SetSourceRGBA(.11, .12, .13, dimAlpha);
+            context.Save();
             context.Rectangle(Boundary);
+            context.Rectangle(ssRect);
+            context.FillRule = c.FillRule.EvenOdd;
             context.Fill();
-            context.SetSourceRGBA(.1, .5, .2, .5);
-            context.Ellipse(Boundary);
-            context.Fill();
+            context.Restore();
 
-            context.LineWidth = 1;
-            context.SetSourceRGB(.3, 1, .6);
-            context.Circle(startPoint, endPoint);
+            if (drawType == DrawType.None) {
+                BoundaryAlpha += BoundaryAlphaIncreasing ? .024 : -.024;
+                if (BoundaryAlpha > 1) BoundaryAlphaIncreasing = false;
+                else if (BoundaryAlpha < .3) BoundaryAlphaIncreasing = true;
+            }
+            mathCommon.ForceInRange(ref BoundaryAlpha, (.3, 1));
+            dashesOffset += .5;
+            if (dashesOffset >= 20) dashesOffset = 0;
+            context.SetDash(new double[] { 2, 6, 2 }, dashesOffset);
+
+            context.LineWidth = 3;
+            c.Rectangle guideRect = new(Boundary.X - context.LineWidth, Boundary.Y - context.LineWidth, Boundary.Width + context.LineWidth * 2, Boundary.Height + context.LineWidth * 2);
+            context.SetSourceRGBA(.1, .1, .1, BoundaryAlpha - .3);
+            context.Rectangle(guideRect);
             context.Stroke();
 
-            context.SetSourceRGB(.2, .6, .8);
-            context.Circle(Boundary);
-
-            context.Stroke();
-
-            context.SetSourceRGB(.2, .8, .3);
-            context.Text(endPoint, "Super duper text", 30);
-            context.Stroke();
-
-            context.Arrow(startPoint, endPoint);
+            context.LineWidth = 1.8;
+            context.SetSourceRGBA(1, 1, 1, BoundaryAlpha);
+            context.Rectangle(guideRect);
             context.Stroke();
 
             SSDrawingArea.QueueDraw();
 
             context.GetTarget().Dispose();
         }
+        private bool newDrag = false;
         private void Draw_ButtonPressEvent(object sender, ButtonPressEventArgs ev) {
             //? Only accept Button 1 (Left click)
             if (ev.Event.Button != 1) return;
-
-            startPoint = new(ev.Event.X, ev.Event.Y);
-
-            DRAWING = true;
+            newDrag = true;
+            BoundaryAlpha = 1;
         }
 
         private void Draw_MotionNotifyEvent(object sender, MotionNotifyEventArgs ev) {
+            if (newDrag) {
+                startPoint = new(ev.Event.X, ev.Event.Y);
+
+                drawType = DrawType.Select;
+                Program.ssToolbox.Hide();
+                newDrag = false;
+            }
+
             //? Ignore if not drawing
-            if (!DRAWING) return;
+            if (drawType == DrawType.None) return;
 
             //? Update endPoint & Generate bounding rectangle
-            endPoint = new(ev.Event.X, ev.Event.Y);
+            GetSize(out int w, out int h);
+            double endX = ev.Event.X, endY = ev.Event.Y;
+            mathCommon.ForceInRange(ref endX, (0, w));
+            mathCommon.ForceInRange(ref endY, (0, h));
+            endPoint = new(endX, endY);
+
             Boundary = PointsToRectangle.Accurate(startPoint, endPoint);
+
         }
 
         private void Draw_ButtonReleaseEvent(object sender, ButtonReleaseEventArgs ev) {
             //? Only accept Button 1 (Left click)
             if (ev.Event.Button != 1) return;
+            drawType = DrawType.None;
+            UpdateToolbox();
+        }
 
-            //endPoint = new(ev.Event.X, ev.Event.Y);
+        private void UpdateToolbox() {
+            Program.ssToolbox.Show();
+            Program.ssToolbox.KeepAbove = true;
+            Program.ssToolbox.Decorated = false;
 
-            DRAWING = false;
+            // No need for the toolbox width currently (discarded ( _ ))
+            Program.ssToolbox.GetSize(out _, out int toolboxH);
+
+            //? Move to the top-left side of the selection (Boundary)
+            Program.ssToolbox.Move((int)Boundary.X, (int)(Boundary.Y - toolboxH));
+
+            // Get and recalculate the old values (toolboxH)
+            Program.ssToolbox.GetPosition(out int toolboxX, out int toolboxY);
+            Program.ssToolbox.GetSize(out int toolboxW, out toolboxH);
+
+            (int x, int y) offset = (0, -10);
+            //? Avoid overlapping the selection (Boundary)
+            if (toolboxY < 0) {
+                offset.y = 10;
+                toolboxY = (int)(Boundary.Y + Boundary.Height);
+            }
+            if (toolboxX < 0) {
+                offset.x = 10;
+                toolboxX = (int)(Boundary.X + Boundary.Width);
+            }
+            //? Apply the offset
+            toolboxX += offset.x; toolboxY += offset.y;
+
+            GetSize(out int w, out int h);
+
+            //? Avoid clipping out of the screen
+            mathCommon.ForceInRange(ref toolboxX, (0, w - toolboxW));
+            mathCommon.ForceInRange(ref toolboxY, (0, h - toolboxH));
+
+            Program.ssToolbox.Move(toolboxX, toolboxY);
         }
     }
 }
