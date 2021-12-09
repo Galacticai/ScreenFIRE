17064168b8cb0bd27fc9cf2260af9f88e15d057a
XEROling <nhkomaiha@gmail.com>
2021-12-09 10:22:06 +0200

    Add `Max` and `Min` functions

--- ScreenFIRE/Modules/Companion/math/mathCommon.cs
+++ ScreenFIRE/Modules/Companion/math/mathCommon.cs
@@ -1,9 +1,26 @@
-﻿namespace ScreenFIRE.Modules.Companion.math {
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ScreenFIRE.Modules.Companion.math {
 
     static class mathCommon {
+        public static type Max<type>(params type[] target) {
+            if (target.Length == 1) return target[0];
+            List<type> targetList = new();
+            foreach (var item in target)
+                targetList.Add(item);
+            return targetList.Max();
+        }
+        public static type Min<type>(params type[] target) {
+            if (target.Length == 1) return target[0];
+            List<type> targetList = new();
+            foreach (var item in target)
+                targetList.Add(item);
+            return targetList.Min();
+        }
 
         /// <summary> Force &amp; set <paramref name="target"/> to be in the <paramref name="boundaries"/> </summary>
-        public static int ForceInRange(ref int target, params (int min, int max)[] boundaries) {
+        public static double ForceInRange(ref double target, params (double min, double max)[] boundaries) {
             foreach (var (min, max) in boundaries) {
                 if (target > max) target = max;
                 if (target < min) target = min;
@@ -11,7 +28,7 @@
             return target;
         }
         /// <summary> Force &amp; set <paramref name="target"/> to be in the <paramref name="boundaries"/> </summary>
-        public static double ForceInRange(ref double target, params (double min, double max)[] boundaries) {
+        public static int ForceInRange(ref int target, params (int min, int max)[] boundaries) {
             foreach (var (min, max) in boundaries) {
                 if (target > max) target = max;
                 if (target < min) target = min;
